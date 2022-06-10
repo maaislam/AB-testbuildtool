8ab7d87d32e4e5e2fa5a1ff8ab0008bed22ab28d
arafatislam <arafat.islam@brainlabsdigital.com>
2022-06-11 00:52:43 +0600

    TP205 initial build

---
+++ clients/Brainlabs/TravisPerkins/TP205/src/lib/components/seeAll.js
@@ -0,0 +1,11 @@
+const renderSeeAll = (id, count) => {
+  const htmlStr = `
+    <div div class="${id}__seefullrange">
+        <div class="${id}__seefullrange--headline">${count} items available. We recommend using the <span>filters to the left</span>, or</div>
+        <div class="${id}__seefullrange--btn">See Full Range</div>
+    </div>`;
+
+  return htmlStr;
+};
+
+export default renderSeeAll;
