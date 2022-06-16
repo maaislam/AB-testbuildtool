39ed9fc1448955dc16edfc479d45a51582cf6f1c
raihankabir36850 <raihanku320@gmail.com>
2022-06-16 15:54:12 +0600

    Create : A new LaRedoute test named 000804-LAR: Discount code box

---
+++ clients/Red-Eye/LaRedoute/LAR-804/src/lib/experiment.js
@@ -0,0 +1,24 @@
+//import { setup, fireEvent } from '../../../../../../globalUtil/trackings/services';
+import shared from './shared/shared';
+
+const { ID, VARIATION } = shared;
+
+export default () => {
+  //setup(); //use if needed
+
+  console.log(ID);
+  // -----------------------------
+  // If control, bail out from here
+  // -----------------------------
+  if (VARIATION == 'control') {
+    return;
+  }
+
+  // -----------------------------
+  // Write experiment code here
+  // -----------------------------
+  // ...
+
+  document.body.classList.add(`${ID}`);
+  document.querySelector(`#giftCodeTitle`).click();
+};
--- config/paths.js
+++ config/paths.js
@@ -4,13 +4,13 @@
     module.exports = {
       // Source files
     
-      src: path.resolve(__dirname, `../clients/Red-Eye/Vuse/BAT-799/src`),
+      src: path.resolve(__dirname, `../clients/Red-Eye/LaRedoute/LAR-804/src`),
     
       // Production build files
-      build: path.resolve(__dirname, `../clients/Red-Eye/Vuse/BAT-799/public`),
+      build: path.resolve(__dirname, `../clients/Red-Eye/LaRedoute/LAR-804/public`),
     
       // Static files that get copied to build folder
-      public: path.resolve(__dirname, `../clients/Red-Eye/Vuse/BAT-799/public`),
+      public: path.resolve(__dirname, `../clients/Red-Eye/LaRedoute/LAR-804/public`),
     
       templateHTML: path.resolve(__dirname, '../template.html'),
     };
