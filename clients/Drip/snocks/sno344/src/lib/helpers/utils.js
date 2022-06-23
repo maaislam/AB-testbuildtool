a86865fd830f0bd90153bf84c23240c0a6815ca8
Karima <karimazafar.yvf@gmail.com>
2022-06-23 19:54:40 +0600

    SNO344 new test

--- clients/Drip/snocks/sno344/src/lib/helpers/utils.js
+++
@@ -1,6 +0,0 @@
-export const isCartDrawer = document.querySelector('#sidebar-cart');
-export const isCartPage = location.pathname.indexOf('/cart') !== -1;
-
-export const thingsToPollFor = isCartDrawer ? ['.Drawer__Container'] :
-    isCartPage ? ['.PageContent'] :
-    null;
