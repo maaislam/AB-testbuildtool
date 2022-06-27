8b0a544d9d92cbfab8efbcf8aca38ea97baa65af
arafatislam <arafat.islam@brainlabsdigital.com>
2022-06-27 19:07:40 +0600

    avon AG095 added + aync capability added

---
+++ clients/Brainlabs/AvonDB/AG095/src/lib/helpers/getCatalog.js
@@ -0,0 +1,26 @@
+const getCatalog = async () => {
+  // eslint-disable-next-line no-undef
+  const { campaign, mrktCd, rep_id } = PDP_MANAGER.API_DATA;
+  const BASE_URL = 'we.avon.digital-catalogue.com';
+  const res = await fetch(
+    `https://api.${BASE_URL}/v2/get-user-id?rep_id=${rep_id}&mrktCd=${mrktCd}`
+  );
+  const resJson = await res.json();
+
+  const catalogRes = await fetch(
+    `https://admin.${BASE_URL}/api/campaigns/${campaign}/brochures/?rep_id=${
+      rep_id || ''
+    }&rep_slug=${resJson.slug}&preventCache=${window.preventCacheId}`
+  );
+
+  const catalogData = await catalogRes.json();
+  console.log(resJson);
+  console.log(catalogData);
+
+  return {
+    repName: resJson.config ? resJson.config.shop_with_my_rep.name : undefined,
+    catalogData,
+  };
+};
+
+export default getCatalog;
