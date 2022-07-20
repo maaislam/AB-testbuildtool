14cc3708be4c053e5b6764f8075f0465262b71ec
arafatislam <maaislam@outlook.com>
2022-07-20 14:43:41 +0600

    eslint updated with airbnb

--- config/paths.js
+++ config/paths.js
@@ -1,15 +1,17 @@
-const path = require('path');
 
-module.exports = {
-  // Source files
-
-  src: path.resolve(__dirname, `../clients/Drip/snocks/sno345/src`),
-
-  // Production build files
-  build: path.resolve(__dirname, `../clients/Drip/snocks/sno345/public`),
-
-  // Static files that get copied to build folder
-  public: path.resolve(__dirname, `../clients/Drip/snocks/sno345/public`),
-
-  templateHTML: path.resolve(__dirname, '../template.html')
-};
+    const path = require('path');
+    
+    module.exports = {
+      // Source files
+    
+      src: path.resolve(__dirname, `../clients/Brainlabs/AvonDB/AG100a/src`),
+    
+      // Production build files
+      build: path.resolve(__dirname, `../clients/Brainlabs/AvonDB/AG100a/public`),
+    
+      // Static files that get copied to build folder
+      public: path.resolve(__dirname, `../clients/Brainlabs/AvonDB/AG100a/public`),
+    
+      templateHTML: path.resolve(__dirname, '../template.html'),
+    };
+    
