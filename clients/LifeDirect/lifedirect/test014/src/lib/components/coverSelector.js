e0fa6e50b97349bfb3ac8f1bb345942c00c97636
arafatislam <maaislam@outlook.com>
2022-09-13 18:07:18 +0600

    lifedirect tes014 initial build

---
+++ clients/LifeDirect/lifedirect/test014/src/lib/components/coverSelector.js
@@ -0,0 +1,36 @@
+/*eslint-disable object-curly-newline */
+import coverUsp from './coverUsp';
+
+const coverSelector = (id, coverData, children) => {
+  const { coverIcon, coverTitle, coverType, coverBtnAction, coverBtnText, uspTick, uspsData } =
+    coverData;
+  const childHtml = `<div class="row-2">
+            ${children}
+        </div>`;
+  const htmlStr = `
+    <div class="${id}__coverselector ${coverType}">
+        <div class="row-1">
+            <div class="column-1">
+                <div class="${id}__coverselector--title">
+                    <span class="cover-icon">${coverIcon}</span>
+                    <span class="cover-title">${coverTitle}</span>
+                </div>
+                ${uspsData
+                  .map((uspData) => coverUsp(id, { coverType, uspData, uspTick }))
+                  .join('\n')}
+            </div>
+            <div class="column-2">
+                <div class="${id}__coverselector--btn"
+                 data-action="${coverBtnAction}">
+                    <span class="select">${coverBtnText}</span>
+                    <span class="selected">SELECTED</span>
+                 </div>
+            </div>
+        </div>
+        ${children ? childHtml : ''}
+    </div>`;
+
+  return htmlStr.trim();
+};
+
+export default coverSelector;
--- config/paths.js
+++ config/paths.js
@@ -4,13 +4,13 @@
     module.exports = {
       // Source files
     
-      src: path.resolve(__dirname, `../clients/Brainlabs/TravisPerkins/TP244/src`),
+      src: path.resolve(__dirname, `../clients/LifeDirect/lifedirect/test014/src`),
     
       // Production build files
-      build: path.resolve(__dirname, `../clients/Brainlabs/TravisPerkins/TP244/public`),
+      build: path.resolve(__dirname, `../clients/LifeDirect/lifedirect/test014/public`),
     
       // Static files that get copied to build folder
-      public: path.resolve(__dirname, `../clients/Brainlabs/TravisPerkins/TP244/public`),
+      public: path.resolve(__dirname, `../clients/LifeDirect/lifedirect/test014/public`),
     
       templateHTML: path.resolve(__dirname, '../template.html'),
     };
