a9dcdb0f9f2f2a84c0958e4bff681942bec357d5
arafatislam <maaislam@outlook.com>
2022-11-04 14:05:24 +0600

    comment out unused code block

--- config/paths.js
+++ config/paths.js
@@ -3,15 +3,7 @@ const path = require('path');
 module.exports = {
   // Source files
 
-  src: path.resolve(__dirname, `../clients/UseltBetter/glimp/test001/src`),
-
-  src: path.resolve(__dirname, `../clients/UseItBetter/glimp/Glimp002/src`),
-
-  // Production build files
-  build: path.resolve(__dirname, `../clients/UseItBetter/glimp/Glimp002/public`),
-
-  // Static files that get copied to build folder
-  public: path.resolve(__dirname, `../clients/UseItBetter/glimp/Glimp002/public`),
+  src: path.resolve(__dirname, `../clients/123drogisterij/123drogisterij/test002/src`),
 
   // Production build files
   build: path.resolve(__dirname, `../clients/UseltBetter/glimp/test001/public`),
