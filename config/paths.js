df2c0d265efa81796eddbeb4d42e1ba7b1c57a62
arafatislam <maaislam@outlook.com>
2022-11-04 14:00:30 +0600

    comment out unused code block

--- config/paths.js
+++ config/paths.js
@@ -1,35 +1,23 @@
+const path = require('path');
 
-    const path = require('path');
-    
-    module.exports = {
-      // Source files
-    
-<<<<<<< HEAD
-      src: path.resolve(__dirname, `../clients/123drogisterij/123drogisterij/test002/src`),
-=======
+module.exports = {
+  // Source files
 
-      src: path.resolve(__dirname, `../clients/UseltBetter/glimp/test001/src`),
+  src: path.resolve(__dirname, `../clients/UseltBetter/glimp/test001/src`),
 
-      src: path.resolve(__dirname, `../clients/UseItBetter/glimp/Glimp002/src`),
-    
-      // Production build files
-      build: path.resolve(__dirname, `../clients/UseItBetter/glimp/Glimp002/public`),
-    
-      // Static files that get copied to build folder
-      public: path.resolve(__dirname, `../clients/UseItBetter/glimp/Glimp002/public`),
->>>>>>> d2096dd265153842ebdbfdbcc2a25ca6a9e7f04f
-    
-      // Production build files
-      build: path.resolve(__dirname, `../clients/UseltBetter/glimp/test001/public`),
-    
-      // Static files that get copied to build folder
-<<<<<<< HEAD
-      public: path.resolve(__dirname, `../clients/123drogisterij/123drogisterij/test002/public`),
-=======
-      public: path.resolve(__dirname, `../clients/UseltBetter/glimp/test001/public`),
+  src: path.resolve(__dirname, `../clients/UseItBetter/glimp/Glimp002/src`),
 
->>>>>>> d2096dd265153842ebdbfdbcc2a25ca6a9e7f04f
-    
-      templateHTML: path.resolve(__dirname, '../template.html'),
-    };
-    
+  // Production build files
+  build: path.resolve(__dirname, `../clients/UseItBetter/glimp/Glimp002/public`),
+
+  // Static files that get copied to build folder
+  public: path.resolve(__dirname, `../clients/UseItBetter/glimp/Glimp002/public`),
+
+  // Production build files
+  build: path.resolve(__dirname, `../clients/UseltBetter/glimp/test001/public`),
+
+  // Static files that get copied to build folder
+  public: path.resolve(__dirname, `../clients/UseltBetter/glimp/test001/public`),
+
+  templateHTML: path.resolve(__dirname, '../template.html')
+};
