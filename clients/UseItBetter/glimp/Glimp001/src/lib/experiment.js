7766b286b21ef4744d41cd38e9c7151df92db2fe
arafatislam <maaislam@outlook.com>
2022-11-06 02:11:27 +0600

    merge err fix

--- clients/UseItBetter/glimp/Glimp001/src/lib/experiment.js
+++ clients/UseItBetter/glimp/Glimp001/src/lib/experiment.js
@@ -39,7 +39,7 @@ export default () => {
 
     document.querySelector(`.${ID}__switchForm`).addEventListener('submit', (e) => {
       e.preventDefault();
-      newFormHandler(ID, removeCustomForm);
+      newFormHandler(ID);
     });
   } else if (isErrorPage || addressForm) {
     sessionStorage.removeItem(`${ID}__show-new-modal`);
