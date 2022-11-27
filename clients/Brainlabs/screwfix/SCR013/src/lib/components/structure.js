f054f4e65ceae79c1696b9897df0af86a0535cb5
raihankabir36850 <raihanku320@gmail.com>
2022-11-27 16:37:03 +0600

    test done of screwfix SCR013

--- clients/Brainlabs/screwfix/SCR013/src/lib/components/structure.js
+++ clients/Brainlabs/screwfix/SCR013/src/lib/components/structure.js
@@ -1,29 +1,40 @@
-const sizeOption = (src, data) => {
+const sizeOption = (data) => {
   //eslint-disable-next-line no-restricted-syntax
-  let option = '';
-  for (const key in data) {
-    option += `<li><a href="${src}-size-${key}/${data[key].toLowerCase()}" data-size="${
-      data[key]
-    }" >${key}</a></li>`;
-  }
+  const { size, code, link } = data;
+
+  const option = `<li><a href="${link}" data-size="${code}" >${size}</a></li>`;
 
   return option;
 };
 
-const dropdownStr = (id, src, data) => {
+export const dropdownStr = (id, src, data, crossIcon) => {
   return `
     <div class="${id}__dropdown-container lg-24 md-12 cols">
         <span class="dropdown-title">SIZE</span>
         <div class="dropdown-box">
             <div class="dropdown-menu">
                 <ul>
-                    ${sizeOption(src, data)}
+                    ${data.map((item) => item.isDelivery && sizeOption(item)).join('\n')}
                 </ul>
             </div>
-        </div>     
+            <div class="${id}__errorMessage ${id}__disable">
+                <div class="${id}__errorMessage-wrapper">
+                    <div class="${id}__icon">${crossIcon}</div>
+                    <div class="${id}__title">Please choose a size</div>
+                </div>
+                
+        </div>
+        </div>
+            
     </div>
     
     `;
 };
 
-export default dropdownStr;
+export const fakeDeliveryButon = (id) => {
+  return `
+    <button id="product_add_to_trolley_image" alt="Add for Delivery" title="Click here to add this item to your basket for delivery" class="btn btn--lg btn--del fill light ${id}__fakeDelivery">
+        Deliver
+    </button>
+    `;
+};
--- clients/Brainlabs/screwfix/SCR013/src/lib/experiment.js
+++ clients/Brainlabs/screwfix/SCR013/src/lib/experiment.js
@@ -1,7 +1,7 @@
 import { setup, fireEvent } from '../../../../../../globalUtil/trackings/services';
-import { sizeData } from './data';
+import { sizeData, crossIcon } from './data';
 import shared from './shared/shared';
-import dropdownStr from './components/structure';
+import { dropdownStr, fakeDeliveryButon } from './components/structure';
 import clickHandler from './components/handler';
 import obsIntersection from './observeIntersection';
 
@@ -9,8 +9,15 @@ const { ID, VARIATION } = shared;
 
 export default () => {
   setup('Experimentation', `Screwfix - ${ID}`, shared);
+  const selectedSize = window.location.pathname.split(
+    window.location.pathname.match(/(size).\d*\//gi)
+  )[1];
   document.body.addEventListener('click', ({ target }) => {
-    clickHandler(shared, target, fireEvent);
+    clickHandler(shared, target, fireEvent, selectedSize);
+  });
+
+  document.querySelector('.pr__product #qty').addEventListener('keyup', (e) => {
+    fireEvent('User interacts with quantity on pdp', shared);
   });
 
   console.log(ID);
@@ -40,30 +47,68 @@ export default () => {
     const isElem = document.querySelector(`li a[data-size="${size.toUpperCase()}"]`);
     if (isElem) {
       isElem.parentElement.classList.add('selected');
+    } else {
+      document
+        .querySelector(`#add_for_collection_button_${selectedSize.toUpperCase()}`)
+        .insertAdjacentHTML('beforebegin', fakeDeliveryButon(ID));
     }
   };
 
   const pageData = sizeData[window.location.pathname.split('-size')[0]];
   const src = window.location.pathname.split('-size')[0];
-  const selectedSize = window.location.pathname.split(
+  const selectedUrl = window.location.pathname.split(
     window.location.pathname.match(/(size).\d*\//gi)
-  )[1];
+  )[0];
 
-  const availableProduct = () => {
-    console.log('promise exerise', pageData);
-    const modifiedData = [];
+  const availableProduct = async () => {
+    console.log('promise exerise', pageData, selectedUrl);
+    let modifiedData = [];
     for (const key in pageData) {
-      modifiedData.push({ [key]: pageData[key] });
+      modifiedData.push({
+        size: key,
+        code: pageData[key],
+        link: `${selectedUrl}size-${key}/${pageData[key].toLowerCase()}`
+      });
     }
-    console.log(modifiedData);
+
+    const checkElem = await Promise.all(
+      modifiedData.map(async (data) => {
+        const url = data.link;
+        const pr = await fetch(data.link);
+        const responseTxt = await pr.text();
+        return { responseTxt, url };
+      })
+    );
+
+    const data = await Promise.all(
+      checkElem.map((e, i) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(e.responseTxt, 'text/html');
+        const elem = doc.querySelector('#product_add_to_trolley_image');
+        if (elem) {
+          return { isDelivery: true, link: e.url };
+        }
+      })
+    );
+    if (data) {
+      const finalData = modifiedData.map((e, i) => {
+        const isChecking = data.find((element) => element && element.link === e.link);
+        if (isChecking) {
+          e.isDelivery = true;
+        }
+        return e;
+      });
+
+      console.log(finalData);
+
+      document
+        .querySelector('.pr__prices > div')
+        .insertAdjacentHTML('afterend', dropdownStr(ID, src, finalData, crossIcon));
+
+      if (selectedSize) {
+        preSelectFn(selectedSize);
+      }
+    }
   };
   availableProduct();
-
-  document
-    .querySelector('.pr__prices > div')
-    .insertAdjacentHTML('afterend', dropdownStr(ID, src, pageData));
-
-  if (selectedSize) {
-    preSelectFn(selectedSize);
-  }
 };
