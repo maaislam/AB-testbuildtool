34b42c5d7fed15d776fb7d9b534684721c5f0acb
raihankabir36850 <raihanku320@gmail.com>
2023-05-10 02:36:38 +0600

    craete a new test under betting.sc

---
+++ clients/Raketech/betting/bet001/src/lib/helpers/initSwiper.js
@@ -0,0 +1,18 @@
+const initSwiper = (container, initConfig) => {
+  //const { slidesPerView, spaceBetween, direction } = initConfig;
+
+  const baseConfig = {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  };
+
+  const slider = new window.Swiper(`${container}`, Object.assign(baseConfig, initConfig));
+};
+
+export default initSwiper;
