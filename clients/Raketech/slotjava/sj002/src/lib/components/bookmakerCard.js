d3fc816d2e207fe2693c2e2b73c122e87a11512a
Saif Junaid <39622371+Saifuddin105@users.noreply.github.com>
2023-06-13 02:51:04 +0600

    new commits (#6)

---
+++ clients/Raketech/slotjava/sj002/src/lib/components/bookmakerCard.js
@@ -0,0 +1,21 @@
+const bookmakerCard = (id, data) => {
+  const { imgSrc, backColor, slotText, slotName } = data;
+
+  const htmlStr = `
+      <a href="" class="${id}__bookmakercard ${id}__bonus-intent swiper-slide" style="background-color: ${backColor}">
+          <div class="${id}__academy">
+            <div class="${id}__academy-col1">
+              <div class="slot-text">${slotText}</div>
+              <div class="slot-name">${slotName}</div>
+              
+            </div>
+            <div class="${id}__academy-col2">
+              <img src="${imgSrc}" alt="${slotName}">
+            </div>
+          </div>
+      </a>`;
+
+  return htmlStr.trim();
+};
+
+export default bookmakerCard;
---
+++ clients/Raketech/slotjava/sj002/src/lib/configs/bookmakerBonusCarouselConfig.js
@@ -0,0 +1,32 @@
+const bookmakerCarouselConfig = [
+  {
+    imgSrc:
+      'https://raketect-cro-public.s3.ap-southeast-2.amazonaws.com/images/slotjava/academy-01.svg',
+    backColor: '#F5BB03',
+    slotText: 'slots 101',
+    slotName: 'varianza & rtp'
+  },
+  {
+    imgSrc:
+      'https://raketect-cro-public.s3.ap-southeast-2.amazonaws.com/images/slotjava/academy-02.svg',
+    backColor: '#C12635',
+    slotText: 'slots 101',
+    slotName: 'Strategie per giochi di slot'
+  },
+  {
+    imgSrc:
+      'https://raketect-cro-public.s3.ap-southeast-2.amazonaws.com/images/slotjava/academy-03.svg',
+    backColor: '#614383',
+    slotText: 'slots 101',
+    slotName: 'RULLI, LINEE, WILD, SCATTER?'
+  },
+  {
+    imgSrc:
+      'https://raketect-cro-public.s3.ap-southeast-2.amazonaws.com/images/slotjava/academy-04.svg',
+    backColor: '#367019',
+    slotText: 'slots 101',
+    slotName: 'TABELLA PAGAMENTO SLOT'
+  }
+];
+
+export default bookmakerCarouselConfig;
