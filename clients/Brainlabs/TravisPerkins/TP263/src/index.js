5d75c4a0a07a8d1c3cce73b19ef267ed4d170887
maaislam <maaislam@outlook.com>
2023-10-13 09:55:12 +0600

    TP265 build

--- clients/Brainlabs/TravisPerkins/TP263/src/index.js
+++ clients/Brainlabs/TravisPerkins/TP263/src/index.js
@@ -1,4 +1,4 @@
 import activate from './lib/experiment';
 import { pollerLite } from './lib/helpers/utils';
 
-pollerLite(['[data-test-id="header-search-button"] [data-test-id="input-wrapper"]'], activate);
+pollerLite(['[data-test-id="header-search-button"]'], activate);
