2673eaedf4c9e73127f314b8c0a2d12299169f73
SakibAlIslam <sakib35-1680@diu.edu.bd>
2023-10-18 02:23:20 +0600

    TP-263: URL updated

--- clients/Brainlabs/TravisPerkins/TP263/src/lib/helpers/data/data.js
+++ clients/Brainlabs/TravisPerkins/TP263/src/lib/helpers/data/data.js
@@ -2,54 +2,54 @@ export const searchTermSuggestions = {
   'Popular Searches': [
     {
       name: 'Sand',
-      url: 'https://www.travisperkins.co.uk/search/?text=sand',
+      url: '/search/?text=sand',
       searchCategory: 'PopularSearches'
     },
     {
       name: 'Gravel',
-      url: 'https://www.travisperkins.co.uk/search?text=Gravel',
+      url: '/search?text=Gravel',
       searchCategory: 'PopularSearches'
     },
     {
       name: 'Decking',
-      url: 'https://www.travisperkins.co.uk/product/gardens-and-landscaping/decking/c/1500107/',
+      url: '/search/?text=Decking',
       searchCategory: 'PopularSearches'
     },
     {
       name: 'Sharp Sand',
-      url: 'https://www.travisperkins.co.uk/search/?text=sharp%20sand',
+      url: '/search/?text=sharp%20sand',
       searchCategory: 'PopularSearches'
     },
     {
       name: 'Sleepers',
-      url: 'https://www.travisperkins.co.uk/search/?text=Sleepers',
+      url: '/search/?text=Sleepers',
       searchCategory: 'PopularSearches'
     }
   ],
   'Top Categories': [
     {
       name: 'Bagged aggregates',
-      url: 'https://www.travisperkins.co.uk/product/building-materials/aggregates/bagged-aggregates/c/1500050/',
+      url: '/product/building-materials/aggregates/bagged-aggregates/c/1500050/',
       searchCategory: 'TopCategories'
     },
     {
       name: 'Treated Timber',
-      url: 'https://www.travisperkins.co.uk/product/timber-and-sheet-materials/sawn-timber/treated-timber/c/1500002/',
+      url: '/product/timber-and-sheet-materials/sawn-timber/treated-timber/c/1500002/',
       searchCategory: 'TopCategories'
     },
     {
       name: 'Fence Posts',
-      url: 'https://www.travisperkins.co.uk/product/gardens-and-landscaping/fencing-and-trellis/fence-posts/c/1500104/',
+      url: '/product/gardens-and-landscaping/fencing-and-trellis/fence-posts/c/1500104/',
       searchCategory: 'TopCategories'
     },
     {
       name: 'Internal Moulded Doors',
-      url: 'https://www.travisperkins.co.uk/product/doors-windows-and-joinery/internal-doors/internal-moulded-doors/c/1572020/',
+      url: '/product/doors-windows-and-joinery/internal-doors/internal-moulded-doors/c/1572020/',
       searchCategory: 'TopCategories'
     },
     {
       name: 'Cement',
-      url: 'https://www.travisperkins.co.uk/product/building-materials/cements/cement/c/1500040/',
+      url: '/product/building-materials/cements/cement/c/1500040/',
       searchCategory: 'TopCategories'
     }
   ]
