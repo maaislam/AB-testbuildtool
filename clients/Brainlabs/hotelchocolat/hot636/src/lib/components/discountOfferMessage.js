7cb372eaefd3e79bd4e5cded05980be52ea5182d
SakibAlIslam <sakibalislam12345@gmail.com>
2023-11-02 22:38:47 +0600

    Design updated - conditions met pending

--- clients/Brainlabs/hotelchocolat/hot636/src/lib/components/discountOfferMessage.js
+++ clients/Brainlabs/hotelchocolat/hot636/src/lib/components/discountOfferMessage.js
@@ -2,12 +2,12 @@ import { cartBagIcon } from '../assets/svg';
 
 const discountOfferMessage = (id, isThresholdMet, deductedPrice) => {
     const message = isThresholdMet
-        ? `<span>Spend £30, get 15% off</span> - You’re just <span>£${deductedPrice} away</span>`
+        ? `<span>Spend £30, get 15% off</span> - You’re just <span>£${deductedPrice}</span> away`
         : `<span class='${id}__gotDiscountMessage'>Congrats! Your order qualifies for 15% off</span>`;
     const htmlStr = `
         <div class="${id}__discountOfferMessage">
             <span>${cartBagIcon}</span>
-            <p class='${id}__discountOfferMessage-text'>${message}</p>
+            <span class='${id}__discountOfferMessage-text'>${message}</span>
         </div>
     `;
 
--- clients/Brainlabs/hotelchocolat/hot636/src/lib/experiment.js
+++ clients/Brainlabs/hotelchocolat/hot636/src/lib/experiment.js
@@ -1,37 +1,65 @@
 import discountOfferMessage from './components/discountOfferMessage';
 import progressBar from './components/progressBar';
+import { observeDOM } from './helpers/utils';
 import setup from './services/setup';
 import shared from './shared/shared';
+
+const showInBasket = true;
+const couponCode = 'HOHOHO';
 
 const { ID, VARIATION } = shared;
 
 const init = () => {
+  const isMobile = () => document.querySelector('nav.stuckMenu');
   const thresholdPrice = 30;
   const location = window.location.href;
-  if (location.includes('/basket')) {
-    const anchorPoint = document.querySelector('#page_heading');
-    const basketTotalPriceText = document.querySelector('tr.order-subtotal td:last-child').textContent;
-    const basketTotalPrice = +basketTotalPriceText.replace('£', '');
+  const basketTotalPriceElem = document.querySelector('.mini-cart-subtotals .subtotal');
+  const basketTotalPrice = basketTotalPriceElem ? +basketTotalPriceElem.textContent.replace('£', '') : 0;
 
-    const isThresholdMet = basketTotalPrice < thresholdPrice;
+  const isThresholdMet = basketTotalPrice < thresholdPrice;
 
-    const progressWidth = (basketTotalPrice / thresholdPrice) * 100;
-    const deductedPrice = isThresholdMet && (thresholdPrice - basketTotalPrice).toFixed(2);
+  const progressWidth = (basketTotalPrice / thresholdPrice) * 100;
+  const deductedPrice = isThresholdMet && (thresholdPrice - basketTotalPrice).toFixed(2);
 
-    const discountProgressCardHtml = `
-      <div class="${ID}__discountProgressCard">
-        ${discountOfferMessage(ID, isThresholdMet, deductedPrice)}
-        ${progressBar(ID, progressWidth.toFixed(2))}
-      </div>
-    `;
+  const discountProgressCardHtml = `
+    <div class="${ID}__discountProgressCard">
+      ${discountOfferMessage(ID, isThresholdMet, deductedPrice)}
+      ${progressBar(ID, progressWidth.toFixed(2))}
+    </div>
+  `;
 
-    if (!document.querySelector(`.${ID}__discountProgressCard`)) {
+  if (showInBasket && location.includes('/basket') && !document.querySelector(`.${ID}__discountProgressCard`)) {
+      const anchorPoint = document.querySelector('#page_heading');
+      const appendPosition = isMobile() ? 'beforebegin' : 'afterend';
+      anchorPoint.insertAdjacentHTML(appendPosition, discountProgressCardHtml);
+      document.querySelector(`.${ID}__discountProgressCard`).classList.add(`${ID}__basketProgressCard`);
+
+      if (isThresholdMet) return;
+
+      const couponInput = document.querySelector('.voucher-field input');
+      const couponApplyBtn = document.querySelector('#add-coupon');
+      const couponError = document.querySelector('.cart-coupon-code .coupon-error');
+
+      if (couponError) return;
+      //not working
+      couponInput.value = couponCode;
+      couponApplyBtn.click();
+  } else if (!location.includes('/checkout') && !document.querySelector(`.${ID}__miniCartProgressCard`) && basketTotalPrice > 0) {
+      const anchorPoint = document.querySelector('.mini-cart-header');
       anchorPoint.insertAdjacentHTML('afterend', discountProgressCardHtml);
-    }
+
+      const miniCardProgressCard = document.querySelector(`.mini-cart-content .${ID}__discountProgressCard`);
+      miniCardProgressCard.classList.add(`${ID}__miniCartProgressCard`);
   }
 };
 
 export default () => {
   setup();
+  //conditions met
+  if (VARIATION === 'control') return;
   init();
+  const configObj = {
+    childList: true, subtree: false
+  };
+  observeDOM('#mini-cart .drop-down-options', init, configObj);
 };
