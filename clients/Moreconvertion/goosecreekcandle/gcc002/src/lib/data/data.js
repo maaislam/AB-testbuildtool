cf9c36e97e6d9d2373b8329b84cd7b1c2a858b5c
SakibAlIslam <sakibalislam12345@gmail.com>
2023-11-05 18:13:28 +0600

    gcc022- variaiton - 01 development done

---
+++ clients/Moreconvertion/goosecreekcandle/gcc002/src/lib/data/data.js
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        paymentIcon: 'https://i.ibb.co/DzfBQk4/image-4.png',
+        border: false
+    },
+    {
+        paymentIcon: 'https://i.ibb.co/fSLx94B/image-5.png',
+        border: true
+    },
+    {
+        paymentIcon: 'https://i.ibb.co/s9ZF28k/image-6.png',
+        border: true
+    },
+    {
+        paymentIcon: 'https://i.ibb.co/7jJG35M/image-7.png',
+        border: true
+    },
+    {
+        paymentIcon: 'https://i.ibb.co/zbK4p89/image-9.png',
+        border: true
+    },
+    {
+        paymentIcon: 'https://i.ibb.co/pd8rxLh/image-10.png',
+        border: true
+    },
+    {
+        paymentIcon: 'https://i.ibb.co/9gRWHCD/image-11.png',
+        border: false
+    },
+    {
+        paymentIcon: 'https://i.ibb.co/568nPqj/image-12.png',
+        border: true
+    }
+];
---
+++ clients/Moreconvertion/goosecreekcandle/gcc002/src/lib/shared/shared.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ID: "gcc002",
+    VARIATION: "1",
+    CLIENT: "Moreconvertion",
+    SITE: "goosecreekcandle"
+  };
