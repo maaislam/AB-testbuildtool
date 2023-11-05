fa20125db4564f8da8cd2f7d8fcfa23e5bd94342
SakibAlIslam <sakibalislam12345@gmail.com>
2023-11-05 19:30:22 +0600

    variation -2 desktop design done

--- clients/Moreconvertion/goosecreekcandle/gcc002/src/lib/experiment.js
+++ clients/Moreconvertion/goosecreekcandle/gcc002/src/lib/experiment.js
@@ -8,17 +8,19 @@ export default () => {
   setup();
   const anchorPoint = document.querySelector('.product-page--submit-action');
 
-  const htmlStr = `<div class='${ID}__wrapper'>
-    ${data.map((item) => {
-      const paymentIcon = `<div class='${ID}__item'>
-        <div class='${ID}__paymentIconWrapper'>
-          <div class='${ID}__paymentIcon ${item.border ? `${ID}__borderedImage` : `${ID}__image`}'>
-            <img src='${item.paymentIcon}' alt='icon' />
+  const htmlStr = `<div class='${ID}__container'>
+      <div class='${ID}__wrapper'>
+      ${data.map((item) => {
+        const paymentIcon = `<div class='${ID}__item'>
+          <div class='${ID}__paymentIconWrapper'>
+            <div class='${ID}__paymentIcon ${item.border ? `${ID}__borderedImage` : `${ID}__image`}'>
+              <img src='${item.paymentIcon}' alt='icon' />
+            </div>
           </div>
-        </div>
-      </div>`;
-      return paymentIcon;
-    }).join('')}
+        </div>`;
+        return paymentIcon;
+      }).join('')}
+    </div>
   </div>`;
 
   if (!document.querySelector(`.${ID}__wrapper`)) {
