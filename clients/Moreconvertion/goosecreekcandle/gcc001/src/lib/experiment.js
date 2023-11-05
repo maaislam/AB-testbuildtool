be9ff25bef85cf642cb424116d02bd1558986bd5
SakibAlIslam <sakibalislam12345@gmail.com>
2023-11-05 14:22:43 +0600

    gcc001 - development done

---
+++ clients/Moreconvertion/goosecreekcandle/gcc001/src/lib/experiment.js
@@ -0,0 +1,31 @@
+import setup from './services/setup';
+import gaTracking from './services/gaTracking';
+import shared from './shared/shared';
+import { data } from './data/data';
+
+const { ID, VARIATION } = shared;
+
+export default () => {
+  setup();
+  console.log(ID);
+
+  const anchorPoint = document.querySelector('.variants_vendor');
+
+  const htmlStr = `<div class='${ID}__uspWrapper'>
+    ${data.map((item) => {
+      const usp = `<div class='${ID}__uspItem'>
+        <div class='${ID}__uspIconWrapper'>
+          <div class='${ID}__uspIcon'>
+            ${item.uspIcon}
+          </div>
+        </div>
+        <div class='${ID}__uspTitle'>
+          ${item.uspTitle}
+        </div>
+      </div>`;
+      return usp;
+    }).join('')}
+  </div>`;
+
+  anchorPoint.insertAdjacentHTML('beforebegin', htmlStr);
+};
