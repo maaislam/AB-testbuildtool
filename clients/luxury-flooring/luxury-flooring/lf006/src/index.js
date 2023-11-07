14d2d6bf93a9a4d71d7583d7029ef73aa26d9836
SakibAlIslam <sakibalislam12345@gmail.com>
2023-11-07 19:40:31 +0600

    check isPDP

--- clients/luxury-flooring/luxury-flooring/lf006/src/index.js
+++ clients/luxury-flooring/luxury-flooring/lf006/src/index.js
@@ -1,4 +1,6 @@
 import activate from './lib/experiment';
 import { pollerLite } from './lib/helpers/utils';
 
-pollerLite(['.sp_accordion .trigger em'], activate);
+if (document.body.classList.contains('catalog-product-view')) {
+    pollerLite(['.sp_accordion .trigger em'], activate);
+}
--- clients/luxury-flooring/luxury-flooring/lf006/src/lib/experiment.js
+++ clients/luxury-flooring/luxury-flooring/lf006/src/lib/experiment.js
@@ -3,8 +3,7 @@ import shared from './shared/shared';
 
 const { ID } = shared;
 
-export default () => {
-  setup();
+const init = () => {
   const anchorPoint = document.querySelector('header.page-header');
   const accordionTextElem = document.querySelector('.sp_accordion .trigger em');
   const accordionText = accordionTextElem.textContent;
@@ -14,4 +13,9 @@ export default () => {
   </div>`;
 
   anchorPoint.insertAdjacentHTML('beforebegin', announcementBanner);
+};
+
+export default () => {
+  setup();
+  init();
 };
