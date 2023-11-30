1bc99fe0a92fe7157fac6e8a475d83c72214df62
SakibAlIslam <sakibalislam12345@gmail.com>
2023-11-30 06:46:28 +0600

    123 - test012: Done

--- clients/123drogisterij/123drogisterij/test012/src/lib/experiment.js
+++ clients/123drogisterij/123drogisterij/test012/src/lib/experiment.js
@@ -1,6 +1,5 @@
 import modal from './components/modal';
 import modalContent from './components/modalContent';
-import qtyInput from './components/qtyInput';
 import getProductInfo from './helpers/getProductData';
 import { formatPrice } from './helpers/utils';
 import setup from './services/setup';
@@ -9,9 +8,7 @@ import shared from './shared/shared';
 
 const { ID } = shared;
 
-export default () => {
-  setup();
-  console.log(ID);
+const init = () => {
   const cartItems = document.querySelectorAll('.cart.item .item-info');
 
   const fakeButton = (
@@ -25,42 +22,42 @@ export default () => {
 
   cartItems.forEach((cartItem) => {
     if (cartItem.querySelector(`.${ID}__openmodal`)) return;
-    //const sku = anchorPoint.querySelector('form').getAttribute('data-product-sku');
     const sku = cartItem.querySelector('td .control.qty input.qty').getAttribute('data-cart-item-id');
     const anchorPoint = cartItem.querySelector('.col.item .product-item-details .product-item-name');
 
     anchorPoint.insertAdjacentHTML('beforeend', fakeButton(ID, sku));
-    // cartItem
-    //   .querySelector('.action.tocart')
-    //   .insertAdjacentHTML('beforebegin', qtyInput(ID, sku));
   });
+};
+
+export default () => {
+  setup();
+  if (!window.location.href.includes('/checkout/cart')) return;
+
+  init();
 
   document.body.addEventListener('click', (e) => {
     const { target } = e;
     //console.log('🚀target:', target);
 
     if (target.closest(`.${ID}__atc`)) {
+      document.querySelector('[data-cart-item-update]').click();
       //get sku
-      const modalForm = target.closest('.ppatc__popup-form');
-      const variantsWrapper = modalForm.querySelector(`.${ID}__variants`);
+      //const modalForm = target.closest('.ppatc__popup-form');
+      //const variantsWrapper = modalForm.querySelector(`.${ID}__variants`);
 
-      const sku = variantsWrapper.getAttribute('data-active-sku');
-      const primaryForm = document.querySelector(`form[data-product-sku="${sku}"]`);
-      primaryForm.querySelector('button[type="submit"]').click();
+      //const sku = variantsWrapper.getAttribute('data-active-sku');
+      //const primaryForm = document.querySelector(`form[data-product-sku="${sku}"]`);
+      //primaryForm.querySelector('button[type="submit"]').click();
       //submit form using js .submit()
     } else if (target.closest(`.${ID}__denyoffer`)) {
-      const modalForm = target.closest('.ppatc__popup-form');
-      const defaultLabel = modalForm.querySelector('label[data-quantity="1"]');
-      // const modalSubmitBtn = modalForm.querySelector(`.${ID}__atc`);
-      defaultLabel.click();
-      // modalSubmitBtn.click();
+      document.body.classList.remove('ppatc__popup-enabled');
     } else if (target.closest(`.${ID}__openmodal`)) {
       const closestParent = target.closest('.product-item-name');
 
       const prodUrl = closestParent?.querySelector('a')?.getAttribute('href');
       if (!prodUrl) return;
       getProductInfo(prodUrl).then((productsData) => {
-        console.log('productsData: ', productsData);
+        //console.log('productsData: ', productsData);
         if (productsData.variants.length > 1) {
           //update modal
           const { sku } = target.closest('button').dataset;
@@ -68,7 +65,7 @@ export default () => {
           const modalInnerContent = document.querySelector('.ppatc__popup-items');
           modalInnerContent.innerHTML = '';
           modalInnerContent.insertAdjacentHTML('afterbegin', modalContent(ID, productsData, sku));
-          modalInnerContent.querySelector('label[data-quantity="1"]')?.click();
+          modalInnerContent.querySelector('.highest_badge').closest(`.${ID}__variant`)?.click();
           modalInnerContent.setAttribute('data-href', prodUrl);
           document.body.classList.add('ppatc__popup-enabled');
 
@@ -81,7 +78,6 @@ export default () => {
       const closestLabel = target.closest('label');
       const labelsWrapper = target.closest(`.${ID}__variants`);
       const modalBody = target.closest('.ppatc__popup-form');
-      const sku = labelsWrapper.dataset.activeSku;
 
       const labelQty = closestLabel.dataset.quantity;
       const labelPrice = closestLabel.dataset.price;
@@ -89,8 +85,15 @@ export default () => {
         item.classList.remove('active');
       });
       closestLabel.classList.add('active');
-      // const activeInput = document.querySelector(`input[data-sku="${sku}"]`);
-      // activeInput.value = labelQty;
+      //const activeInput = document.querySelector(`input[data-sku="${sku}"]`);
+      //activeInput.value = labelQty;
+      setTimeout(() => {
+        const prodUrl = document.querySelector('.ppatc__popup-items').dataset.href;
+        const busketRowLink = document.querySelector(`table .item-info .product-item-name [href*="${prodUrl}"]`);
+        const busketRow = busketRowLink.closest('.item-info');
+        const rowQtyInput = busketRow.querySelector('.input-text.qty');
+        rowQtyInput.value = labelQty;
+      }, 1000);
 
       modalBody.querySelector('.price').innerText = formatPrice(labelPrice);
     } else if (target.closest('.ppatc__popup-link')) {
