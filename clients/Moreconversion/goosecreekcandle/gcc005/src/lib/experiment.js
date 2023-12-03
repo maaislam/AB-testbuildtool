1df179a52b8e11f0710e5a1ce58d4aefce74f0be
SakibAlIslam <sakibalislam12345@gmail.com>
2023-12-03 18:51:48 +0600

    gcc005 - done

--- clients/Moreconversion/goosecreekcandle/gcc005/src/lib/experiment.js
+++ clients/Moreconversion/goosecreekcandle/gcc005/src/lib/experiment.js
@@ -1,21 +1,26 @@
+import addScentBadge from './helpers/addScentBadge';
+import { onUrlChange } from './helpers/utils';
 import setup from './services/setup';
-import gaTracking from './services/gaTracking';
 import shared from './shared/shared';
 
-const { ID, VARIATION } = shared;
+const { ID } = shared;
 
 export default () => {
-  setup(); //use if needed
-  gaTracking('Conditions Met'); //use if needed
-  console.log(ID);
-  //-----------------------------
-  //If control, bail out from here
-  //-----------------------------
-  //if (VARIATION === 'control') {
-  //}
+  setup();
 
-  //-----------------------------
-  //Write experiment code here
-  //-----------------------------
-  //...
+  const paramKey = 'filter.v.scent';
+
+  const getSearchParamValue = (paramName) => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const paramValue = queryParams.get(paramName);
+    return paramValue;
+  };
+
+  onUrlChange(() => {
+    const { search } = window.location;
+    if (search.includes(paramKey)) {
+      const searchParamValue = getSearchParamValue(paramKey);
+      addScentBadge(ID, searchParamValue);
+    }
+  });
 };
