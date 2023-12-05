f17f1616724a5c8c4ef4aabf075cc60b714ed248
maaislam <maaislam@outlook.com>
2023-12-06 05:07:40 +0600

    lx flooring 139 test build

--- clients/Moreconversion/infiniteAge/ig004/src/lib/experiment.js
+++ clients/Moreconversion/infiniteAge/ig004/src/lib/experiment.js
@@ -1,33 +1,78 @@
 import setup from './services/setup';
-import gaTracking from './services/gaTracking';
+
 import shared from './shared/shared';
 
-const { ID, VARIATION } = shared;
+const { ID } = shared;
 
 export default () => {
   setup(); //use if needed
-  gaTracking('Conditions Met'); //use if needed
-  console.log(ID);
-  //-----------------------------
-  //If control, bail out from here
-  //-----------------------------
-  //if (VARIATION === 'control') {
-  //}
+  //const extractNumber = (str) => {
+  //const num = str.match(/\d/g);
+  //return num.join('');
+  //};
 
-  //-----------------------------
-  //Write experiment code here
-  //-----------------------------
-  //...
-  const controlSider = document.querySelector('[id^="MediaGallery-template--"]');
-  const imageElemWrappers = controlSider.querySelectorAll('.product__media');
-  const imageElemsArray = Array.from(imageElemWrappers).map((item) => item.querySelector('img'));
-  const splicedArr = imageElemsArray.splice(2);
-  splicedArr.push(imageElemsArray[0]);
-  splicedArr.push(imageElemsArray[1]);
-  //console.log('🚀 ~ file: experiment.js:48 ~ splicedArr:', splicedArr);
-  imageElemWrappers.forEach((imageElemWrapper, index) => {
-    //eslint-disable-next-line no-param-reassign
-    imageElemWrapper.innerHTML = '';
-    imageElemWrapper.appendChild(splicedArr[index]);
-  });
+  const isMobile = window.innerWidth < 576;
+
+  if (!isMobile) return;
+
+  const pkgStripOneTime = document.querySelector('.oneetime .pkg_strip');
+  const pkgStripSub = document.querySelector('.subscript .pkg_strip');
+
+  //clonedPkgStrip.classList.add(`${ID}__cloned-pkgstrip`);
+
+  const oneTimePacks = document.querySelectorAll('.oneetime [data-val]');
+  if (oneTimePacks.length === 0) return;
+  const firstOneTime = oneTimePacks[0];
+  firstOneTime.parentNode.insertBefore(firstOneTime, oneTimePacks[2]);
+  firstOneTime.insertAdjacentElement('beforebegin', pkgStripOneTime);
+
+  const subPacks = document.querySelectorAll('.subscript [data-val]');
+  if (subPacks.length === 0) return;
+  const firstSub = subPacks[0];
+  firstSub.parentNode.insertBefore(firstSub, subPacks[2]);
+  firstSub.insertAdjacentElement('beforebegin', pkgStripSub);
+
+  //const oneTimePacksArr = [...oneTimePacks];
+  //const subPacksArr = [...subPacks];
+
+  //const highestValOneTime = oneTimePacksArr.reduce(
+  //(acc, curr, i) => {
+  //if (window.getComputedStyle(curr).display === 'none') return acc;
+
+  //const title = curr.querySelector('.package-card__title ').textContent;
+  //const getVal = Number(extractNumber(title));
+  //curr.classList.add(`${ID}__onetime-${i + 1}`);
+  //if (getVal > acc?.val) {
+  //acc.val = getVal;
+  //acc.el = curr;
+  //}
+  //return acc;
+  //},
+  //{ val: 0, el: null }
+  //);
+
+  //const highestValSub = subPacksArr.reduce(
+  //(acc, curr, i) => {
+  //if (window.getComputedStyle(curr).display === 'none') return acc;
+  //const title = curr.querySelector('.package-card__title ').textContent;
+  //const getVal = Number(extractNumber(title));
+
+  //curr.classList.add(`${ID}__sub-${i + 1}`);
+
+  //if (getVal > acc?.val) {
+  //acc.val = getVal;
+  //acc.el = curr;
+  //}
+  //return acc;
+  //},
+  //{ val: 0, el: null }
+  //);
+  //console.log('highestValOneTime:', highestValOneTime);
+  //console.log('highestValSub:', highestValSub);
+
+  //const highestValOneTimeEl = highestValOneTime.el;
+  //const highestValSubEl = highestValSub.el;
+
+  //highestValOneTimeEl?.classList.add(`${ID}__highest-val-one-time`);
+  //highestValSubEl?.classList.add(`${ID}__highest-val-sub`);
 };
