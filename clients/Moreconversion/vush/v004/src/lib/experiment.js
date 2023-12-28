ce96ae3bc5f46ac48d9fce95bbc8506b5267f453
SakibAlIslam <sakibalislam12345@gmail.com>
2023-12-28 09:00:28 +0600

    test-3 - v005 - done

--- clients/Moreconversion/vush/v004/src/lib/experiment.js
+++ clients/Moreconversion/vush/v004/src/lib/experiment.js
@@ -1,9 +1,9 @@
 import setup from './services/setup';
-import gaTracking from './services/gaTracking';
+//import gaTracking from './services/gaTracking';
 import shared from './shared/shared';
 import { observeDOM } from './helpers/utils';
 
-const { ID, VARIATION } = shared;
+const { ID } = shared;
 
 const init = () => {
   const customConfig = {
@@ -19,7 +19,7 @@ const init = () => {
       if (form) {
         form.parentElement.classList.add(`${ID}__hide`);
       }
-      console.log('mutation', form);
+      //console.log('mutation', form);
     }, 250);
   }, customConfig);
 };
---
+++ clients/Moreconversion/vush/v005/src/lib/experiment.js
@@ -0,0 +1,22 @@
+import setup from './services/setup';
+import gaTracking from './services/gaTracking';
+import shared from './shared/shared';
+
+const { ID, VARIATION } = shared;
+
+const init = () => {
+  const anchorPoint = document.querySelector('.announcement-bar');
+
+  const htmlStr = `<div class='${ID}__announcementBar'>
+    <div class='${ID}__announcementBar__content'>
+      <span>FREE EXPRESS SHIPPING ON ALL ORDERS</span>
+    </div>
+  </div>`;
+  anchorPoint.insertAdjacentHTML('beforebegin', htmlStr);
+};
+
+export default () => {
+  setup(); //use if needed
+
+  init();
+};
