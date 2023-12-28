f0359ee0f66645741cb344129bcf184b9591c8fe
SakibAlIslam <sakibalislam12345@gmail.com>
2023-12-28 18:11:59 +0600

    moreconversion - test-1 developed

---
+++ clients/Moreconversion/vush/v006/src/lib/experiment.js
@@ -0,0 +1,23 @@
+import setup from './services/setup';
+import gaTracking from './services/gaTracking';
+import shared from './shared/shared';
+
+const { ID, VARIATION } = shared;
+
+const init = () => {
+  const isMobile = window.innerWidth < 768;
+  const searchBarSelector = isMobile ? '.header__row-mobile .header__icon-touch--search' : '.header__row-segment-desktop .header__icon-touch--search';
+  const searchBar = document.querySelector(`${searchBarSelector}`);
+
+  searchBar.classList.add(`${ID}__searchBar`);
+
+  const htmlStr = `<span class='${ID}__placeHolder'>Search for Empress, Vibrator, Lube..</span>`;
+  searchBar.insertAdjacentHTML('afterbegin', htmlStr);
+};
+
+export default () => {
+  setup();
+
+  console.log(ID);
+  init();
+};
