3184d883a728e520915a3a9d0397e884263117e0
SakibAlIslam <sakibalislam12345@gmail.com>
2024-01-10 05:40:16 +0600

    ata004 done

--- clients/Moreconvertion/Arrowhead/ata004/src/lib/experiment.js
+++ clients/Moreconvertion/Arrowhead/ata004/src/lib/experiment.js
@@ -1,13 +1,16 @@
 import setup from './services/setup';
-import shared from './shared/shared';
+import shared, { VARIATION } from './shared/shared';
 import sizeGuide from './components/sizeGuide';
 
 const { ID } = shared;
 
 const init = () => {
-  const anchorPoint = document.querySelector('body .product-description div[data-mce-fragment="1"]');
+  const anchorPointSelector = VARIATION === '1' ? 'product-description div[data-mce-fragment="1"]' : 'product-description h5';
+  const anchorPosition = VARIATION === '1' ? 'beforebegin' : 'afterend';
+  const anchorPoint = document.querySelector(`.${anchorPointSelector}`);
+
   if (!document.querySelector(`.${ID}__sizeGuideContainer`)) {
-    anchorPoint.insertAdjacentHTML('beforebegin', sizeGuide(ID));
+    anchorPoint.insertAdjacentHTML(anchorPosition, sizeGuide(ID));
   }
 };
 
