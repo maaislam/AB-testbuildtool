447f58f9fda355fc83eac16deb31fe96a8c2e633
SakibAlIslam <sakibalislam12345@gmail.com>
2024-01-24 15:15:19 +0600

    Fix text in experiment.js

--- clients/Funnelenvy/Dice/feDice128/src/lib/experiment.js
+++ clients/Funnelenvy/Dice/feDice128/src/lib/experiment.js
@@ -10,15 +10,15 @@ const init = () => {
   const techConnectData = [
     {
       icon: 'https://fe-test-dev.s3.amazonaws.com/Dice/Dice-128/candidatesVerifyIcon.svg',
-      text: 'Find pre-screened tech candidates, many verified'
+      text: 'Find pre-screened tech candidates, many verified.'
     },
     {
       icon: 'https://fe-test-dev.s3.amazonaws.com/Dice/Dice-128/enhanceJobPostingIcon.svg',
-      text: 'Enhance your job postings'
+      text: 'Enhance your job posting.'
     },
     {
       icon: 'https://fe-test-dev.s3.amazonaws.com/Dice/Dice-128/hireWithConfidenceIcon.svg',
-      text: 'Hire with confidence'
+      text: 'Hire with confidence.'
     }
   ];
   const list = (item) => {
