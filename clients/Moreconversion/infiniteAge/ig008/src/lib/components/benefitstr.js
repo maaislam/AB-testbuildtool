65688bc4e128bafa53623fa4db3723b4f5387957
raihankabir36850 <raihanku320@gmail.com>
2024-02-08 21:57:57 +0600

    new amends done except number 8 of test ig008

--- clients/Moreconversion/infiniteAge/ig008/src/lib/components/benefitstr.js
+++ clients/Moreconversion/infiniteAge/ig008/src/lib/components/benefitstr.js
@@ -3,7 +3,6 @@ import benefitItem from './benefitItem';
 const benefitStr = (ID, benefitArray) => {
   const html = `
     <div class="${ID}__banner_list">
-        <div class="banner_prod-mob showMob"><img src="https://hitsdesignclients.com/InfiniteAge_BlackSeed_Extract/V2/images/mob-prod.png" width="383" height="519" alt="img"></div>
         <ul class="banner__points">
             ${benefitArray.map((item) => benefitItem(item)).join('\n')}
         </ul>
