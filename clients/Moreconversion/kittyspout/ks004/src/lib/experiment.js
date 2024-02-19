0662db8dce184dd9d2911b1cd5609b1f36c2b19b
raihankabir36850 <raihanku320@gmail.com>
2024-02-19 17:57:03 +0600

    create new tests of kittyspout

---
+++ clients/Moreconversion/kittyspout/ks004/src/lib/experiment.js
@@ -0,0 +1,64 @@
+import setup from './services/setup';
+import gaTracking from './services/gaTracking';
+import shared from './shared/shared';
+import { observeDOM } from './helpers/utils';
+import progressBar from './components/progressBar';
+
+const { ID, VARIATION } = shared;
+
+const init = () => {
+  const thresholdPrice = 85;
+  const basketTotalPriceCtrl = document.querySelector(
+    '.rebuy-cart__flyout-subtotal-amount'
+  ).textContent;
+
+  const match = basketTotalPriceCtrl.match(/\d+\.\d+/);
+  const basketTotalPrice = match ? +match[0] : 0;
+
+  const isThresholdMet = basketTotalPrice < thresholdPrice;
+  if (isThresholdMet) {
+    //fireEvent('user meets the threshold');
+  }
+  console.log(basketTotalPrice);
+
+  const progressWidth = (basketTotalPrice / thresholdPrice) * 100;
+  const deductedPrice = isThresholdMet && (thresholdPrice - basketTotalPrice).toFixed(2);
+
+  const shippingInfoHtml = `
+    <div class='${ID}__shippingInfo'>
+      <div class="${ID}__discountProgressCard">
+        ${progressBar(ID, progressWidth.toFixed(2), deductedPrice, isThresholdMet)}
+      </div>
+    </div>
+  `;
+
+  const anchorPoint = document.querySelector('.rebuy-cart__flyout-content.has-items');
+
+  if (document.querySelector(`.${ID}__shippingInfo`)) {
+    document.querySelector(`.${ID}__shippingInfo`).remove();
+  }
+
+  anchorPoint.insertAdjacentHTML('afterbegin', shippingInfoHtml);
+};
+
+export default () => {
+  setup();
+  console.log(ID);
+  const configObj = {
+    childList: true,
+    subtree: false,
+    attributes: false
+  };
+  observeDOM(
+    'body',
+    () => {
+      if (document.querySelector('.rebuy-cart__flyout-content.has-items')) {
+        init();
+      }
+      if (document.querySelector('.rebuy-cart__flyout-content.no-items')) {
+        document.querySelector(`.${ID}__shippingInfo`)?.remove();
+      }
+    },
+    configObj
+  );
+};
---
+++ clients/Moreconversion/kittyspout/ks005/src/index.js
@@ -0,0 +1,5 @@
+import activate from './lib/experiment';
+import { pollerLite } from './lib/helpers/utils';
+
+pollerLite(['body'], activate);
+// .variant-wrapper fieldset[name="Model"]'
