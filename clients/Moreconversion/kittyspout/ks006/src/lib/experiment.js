cacbbcc19279d125239de71e0b4e8e52edbad73f
SakibAlIslam <sakibalislam12345@gmail.com>
2024-02-19 18:36:40 +0600

    cat drinking water test done

---
+++ clients/Moreconversion/kittyspout/ks006/src/lib/experiment.js
@@ -0,0 +1,21 @@
+import setup from './services/setup';
+import shared from './shared/shared';
+
+const { ID } = shared;
+
+const init = () => {
+  const anchorPoint = document.querySelector('.shopify-section.section');
+  const catDrinkingWater = document.querySelectorAll('.shopify-section .index-section .feature-row-wrapper')[2];
+
+  if (anchorPoint && catDrinkingWater) {
+    const catDrinkingWaterSection = catDrinkingWater.closest('.shopify-section');
+    catDrinkingWaterSection.classList.add(`${ID}__catDrinkingWaterSection`);
+    anchorPoint.insertAdjacentElement('afterend', catDrinkingWaterSection);
+  }
+};
+
+export default () => {
+  setup(); //use if needed
+
+  init();
+};
