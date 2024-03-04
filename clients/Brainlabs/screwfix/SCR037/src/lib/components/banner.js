3ff9fcae2fd9ec6fed37121692c49fc9ea6e1cad
raihankabir36850 <raihanku320@gmail.com>
2024-03-04 15:16:20 +0600

    clean the codebase of scr037 test.

--- clients/Brainlabs/screwfix/SCR037/src/lib/components/banner.js
+++ clients/Brainlabs/screwfix/SCR037/src/lib/components/banner.js
@@ -1,47 +1,47 @@
 const banner = (ID) => {
   const html = `
         <div class="_9F87cc ROCjU1 ${ID}__banner" data-qaid="header-bottom-merchArea-0">
-        <section style="background-color: #2a861a">
-            <a
-            class="kND5MA ${ID}__banner-link"
-            tabindex="0"
-            href="https://www.screwfix.com/search?search=newonlinerangespromo"
-            >
-            <div class="wrapper">
-                <span
-                style="color: rgb(255, 255, 255); font-family: screwfix_betaregular, arial, sans-serif"
-                >SHOP OUR NEW IN</span
+            <section style="background-color: #2a861a">
+                <a
+                class="kND5MA ${ID}__banner-link"
+                tabindex="0"
+                href="https://www.screwfix.com/search?search=newonlinerangespromo"
                 >
-                <span
-                style="
-                    border-bottom: 8px solid transparent;
-                    border-top: 8px solid transparent;
-                    border-left: 8px solid rgb(255, 255, 255);
-                    margin: 0px 3px -1px;
-                    display: inline-block;
-                "
-                ></span>
-                <span
-                style="
-                    border-bottom: 8px solid transparent;
-                    border-top: 8px solid transparent;
-                    border-left: 8px solid rgb(255, 255, 255);
-                    margin: 0px 3px -1px;
-                    display: inline-block;
-                "
-                ></span>
-                <span
-                style="
-                    border-bottom: 8px solid transparent;
-                    border-top: 8px solid transparent;
-                    border-left: 8px solid rgb(255, 255, 255);
-                    margin: 0px 3px -1px;
-                    display: inline-block;
-                "
-                ></span>
-            </div>
-            </a>
-        </section>
+                    <div class="wrapper">
+                        <span
+                        style="color: rgb(255, 255, 255); font-family: screwfix_betaregular, arial, sans-serif"
+                        >SHOP OUR NEW IN</span
+                        >
+                        <span
+                        style="
+                            border-bottom: 8px solid transparent;
+                            border-top: 8px solid transparent;
+                            border-left: 8px solid rgb(255, 255, 255);
+                            margin: 0px 3px -1px;
+                            display: inline-block;
+                        "
+                        ></span>
+                        <span
+                        style="
+                            border-bottom: 8px solid transparent;
+                            border-top: 8px solid transparent;
+                            border-left: 8px solid rgb(255, 255, 255);
+                            margin: 0px 3px -1px;
+                            display: inline-block;
+                        "
+                        ></span>
+                        <span
+                        style="
+                            border-bottom: 8px solid transparent;
+                            border-top: 8px solid transparent;
+                            border-left: 8px solid rgb(255, 255, 255);
+                            margin: 0px 3px -1px;
+                            display: inline-block;
+                        "
+                        ></span>
+                    </div>
+                </a>
+            </section>
         </div>
     `;
 
--- clients/Brainlabs/screwfix/SCR037/src/lib/experiment.js
+++ clients/Brainlabs/screwfix/SCR037/src/lib/experiment.js
@@ -6,19 +6,10 @@ import banner from './components/banner';
 const { ID, VARIATION } = shared;
 
 export default () => {
-  setup(); //use if needed
-  gaTracking('Conditions Met'); //use if needed
+  setup();
+  gaTracking('Conditions Met');
   console.log(ID);
-  //-----------------------------
-  //If control, bail out from here
-  //-----------------------------
-  //if (VARIATION === 'control') {
-  //}
 
-  //-----------------------------
-  //Write experiment code here
-  //-----------------------------
-  //...
   const anchorPoint = document.querySelector('header div[data-qaid="header-bottom-merchArea-0"]');
   if (!document.querySelector(`.${ID}__banner`)) {
     anchorPoint.insertAdjacentHTML('beforebegin', banner(ID));
