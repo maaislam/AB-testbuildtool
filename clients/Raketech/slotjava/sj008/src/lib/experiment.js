ddb69d5441405c92976aa001c6a953edc3f98d21
raihankabir36850 <raihanku320@gmail.com>
2024-03-08 21:08:34 +0600

    mew test done sj008

--- clients/Raketech/slotjava/sj008/src/lib/experiment.js
+++ clients/Raketech/slotjava/sj008/src/lib/experiment.js
@@ -4,19 +4,25 @@ import shared from './shared/shared';
 
 const { ID, VARIATION } = shared;
 
-const init = () => {
-  const anchorPoint = document.querySelector('section#table .container');
-  const rows = [...document.querySelectorAll('table.casino-table tr.casino-table__data-row')];
-  const ITEMS_PER_PAGE = 7;
+const anchorPoint = document.querySelector('section#table .container');
+const rows = [...document.querySelectorAll('table.casino-table tr.casino-table__data-row')];
+const ITEMS_PER_PAGE = 7;
 
+const init = () => {
   //show more button
   const htmlStr = `<div class='${ID}__showMoreButtonWrapper'>
     <button class='${ID}__showMoreButton' id="showMoreButton">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" class="${ID}__icon show">
         <mask id="path-1-inside-1_1_477" fill="white">
           <path d="M7.99914 0L15.9991 8L7.99914 16L-0.000863196 8L7.99914 0Z"/>
         </mask>
         <path d="M7.99914 16L5.87782 18.1213L7.99914 20.2426L10.1205 18.1213L7.99914 16ZM13.8778 5.87868L5.87782 13.8787L10.1205 18.1213L18.1205 10.1213L13.8778 5.87868ZM10.1205 13.8787L2.12046 5.87868L-2.12218 10.1213L5.87782 18.1213L10.1205 13.8787Z" fill="#FF7556" mask="url(#path-1-inside-1_1_477)"/>
+      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" class="${ID}__icon less">
+        <mask id="path-1-inside-1_5518_72156" fill="white">
+        <path d="M8 16L-4.00521e-07 8L8 5.47657e-07L16 8L8 16Z"/>
+        </mask>
+        <path d="M8 5.47657e-07L10.1213 -2.12132L8 -4.24264L5.87868 -2.12132L8 5.47657e-07ZM2.12132 10.1213L10.1213 2.12132L5.87868 -2.12132L-2.12132 5.87868L2.12132 10.1213ZM5.87868 2.12132L13.8787 10.1213L18.1213 5.87868L10.1213 -2.12132L5.87868 2.12132Z" fill="#FF7556" mask="url(#path-1-inside-1_5518_72156)"/>
       </svg>
       <span>Mostra altri casinò (${rows.length - ITEMS_PER_PAGE})</span>
     </button>
@@ -32,6 +38,20 @@ const init = () => {
   }
 };
 
+const showAllItems = () => {
+  document.querySelectorAll(`#table.${ID}__show-full .${ID}__hide`).forEach((element) => {
+    element && element.classList.toggle(`${ID}__hide`);
+  });
+  document.querySelector('#showMoreButton span').innerText = 'MOSTRA MENO CASINÒ';
+};
+
+const hideItems = () => {
+  rows.slice(ITEMS_PER_PAGE).forEach((element) => element.classList.toggle(`${ID}__hide`));
+  document.querySelector('#showMoreButton span').innerText = `Mostra altri casinò (${
+    rows.length - ITEMS_PER_PAGE
+  })`;
+};
+
 export default () => {
   setup();
 
@@ -39,21 +59,19 @@ export default () => {
     const { target } = e;
 
     if (target.closest('#showMoreButton')) {
-      //gaTracking('Show All Casinos');
-      document.querySelector('#table table.casino-table').classList.toggle(`${ID}__show-full`);
-      if (document.querySelector(`#table table.casino-table.${ID}__show-full`)) {
-        document
-          .querySelectorAll(`#table table.casino-table.${ID}__show-full .${ID}__hide`)
-          .forEach((element) => {
-            element && element.classList.toggle(`${ID}__hide`);
-          });
+      document.querySelector('#table').classList.toggle(`${ID}__show-full`);
+
+      if (document.querySelector(`#table.${ID}__show-full`)) {
+        showAllItems();
       } else {
-        // document
-        //   .querySelectorAll(`#table table.casino-table.${ID}__show-full`)
-        //   .forEach((element) => {
-        //     element && element.classList.toggle(`${ID}__hide`);
-        //   });
+        hideItems();
       }
+
+      target.closest('#showMoreButton').scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+      });
+      gaTracking('Show All Casinos');
     } else if (target.closest('[href*="visita"]')) {
       const casinoNameElem = target.closest('a[href*="visita"]');
 
--- clients/Raketech/slotjava/sj008/src/lib/services/gaTracking.js
+++ clients/Raketech/slotjava/sj008/src/lib/services/gaTracking.js
@@ -1,13 +1,21 @@
 import { pollerLite } from '../helpers/utils';
+import shared from '../shared/shared';
 
-const gaTracking = (label, action = 'click') => {
-  pollerLite([() => typeof window.ga.getAll === 'function'], () => {
-    window.ga.getAll().forEach((tracker) => {
-      tracker.send('event', {
-        eventCategory: 'funnelenvy',
-        eventAction: action,
-        eventLabel: label
-      });
+const gaTracking = (label) => {
+  const { VARIATION } = shared;
+
+  const GA4_INTERNAL_EXPERIMENT_NUM = '6172706773';
+  const GA4_INTERNAL_EXPERIMENT_ID = `cro-${GA4_INTERNAL_EXPERIMENT_NUM}`;
+  const EXPERIMENT_DEVICE_CATEGORY = 'All Devices';
+
+  pollerLite([() => document.readyState === 'complete'], () => {
+    window.dataLayer.push({
+      event: 'cro_event',
+      event_detail: GA4_INTERNAL_EXPERIMENT_ID,
+      event_category: EXPERIMENT_DEVICE_CATEGORY,
+      event_label: `${GA4_INTERNAL_EXPERIMENT_NUM} | V${
+        VARIATION === 'Control' ? 'C' : VARIATION
+      } | ${label}`
     });
   });
 };
