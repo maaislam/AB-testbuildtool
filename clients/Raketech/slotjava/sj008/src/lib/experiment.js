4d5229a9ed1ab8a4f2afa41857c0e689b35bffac
raihankabir36850 <raihanku320@gmail.com>
2024-03-08 19:00:42 +0600

    fix the folder issue of test014

--- clients/Raketech/slotjava/sj008/src/lib/experiment.js
+++ clients/Raketech/slotjava/sj008/src/lib/experiment.js
@@ -6,6 +6,8 @@ const { ID, VARIATION } = shared;
 
 const init = () => {
   const anchorPoint = document.querySelector('section#table .container');
+  const rows = [...document.querySelectorAll('table.casino-table tr.casino-table__data-row')];
+  const ITEMS_PER_PAGE = 7;
 
   //show more button
   const htmlStr = `<div class='${ID}__showMoreButtonWrapper'>
@@ -16,14 +18,18 @@ const init = () => {
         </mask>
         <path d="M7.99914 16L5.87782 18.1213L7.99914 20.2426L10.1205 18.1213L7.99914 16ZM13.8778 5.87868L5.87782 13.8787L10.1205 18.1213L18.1205 10.1213L13.8778 5.87868ZM10.1205 13.8787L2.12046 5.87868L-2.12218 10.1213L5.87782 18.1213L10.1205 13.8787Z" fill="#FF7556" mask="url(#path-1-inside-1_1_477)"/>
       </svg>
-      <span>Mostra altri casinò</span>
+      <span>Mostra altri casinò (${rows.length - ITEMS_PER_PAGE})</span>
     </button>
   </div>`;
 
-  anchorPoint.insertAdjacentHTML('afterend', htmlStr);
+  if (document.querySelector(`.${ID}__showMoreButtonWrapper`)) {
+    document.querySelector(`.${ID}__showMoreButtonWrapper`).remove();
+  }
 
-  const rows = document.querySelectorAll('table.casino-table tr.casino-table__data-row');
-  const itemsPerPage = 7;
+  if (rows.length > ITEMS_PER_PAGE) {
+    anchorPoint.insertAdjacentHTML('afterend', htmlStr);
+    rows.slice(ITEMS_PER_PAGE).forEach((element) => element.classList.toggle(`${ID}__hide`));
+  }
 };
 
 export default () => {
@@ -33,7 +39,21 @@ export default () => {
     const { target } = e;
 
     if (target.closest('#showMoreButton')) {
-      gaTracking('Show All Casinos');
+      //gaTracking('Show All Casinos');
+      document.querySelector('#table table.casino-table').classList.toggle(`${ID}__show-full`);
+      if (document.querySelector(`#table table.casino-table.${ID}__show-full`)) {
+        document
+          .querySelectorAll(`#table table.casino-table.${ID}__show-full .${ID}__hide`)
+          .forEach((element) => {
+            element && element.classList.toggle(`${ID}__hide`);
+          });
+      } else {
+        // document
+        //   .querySelectorAll(`#table table.casino-table.${ID}__show-full`)
+        //   .forEach((element) => {
+        //     element && element.classList.toggle(`${ID}__hide`);
+        //   });
+      }
     } else if (target.closest('[href*="visita"]')) {
       const casinoNameElem = target.closest('a[href*="visita"]');
 
