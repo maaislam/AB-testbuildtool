1dc532ab2d962b880f908837809219c14751a84d
maaislam <maaislam@outlook.com>
2024-03-15 13:52:41 +0600

    lf143 code cleanup

--- clients/luxury-flooring/luxury-flooring/lf143/src/lib/experiment.js
+++ clients/luxury-flooring/luxury-flooring/lf143/src/lib/experiment.js
@@ -2,6 +2,7 @@ import setup from './services/setup';
 import shared from './shared/shared';
 import orderButton from './components/orderButton';
 import { observeDOM } from './helpers/utils';
+import emitCartDataOnChange from './helpers/emitCartData';
 
 const { ID } = shared;
 const DOM_RENDER_DELAY = 2000;
@@ -25,55 +26,51 @@ const init = (mutation) => {
 };
 
 export default () => {
-  setup();
   const targetPoint = document.querySelector('#sample_addtocart_form');
+  setup();
+  emitCartDataOnChange();
 
-  window.require(['Magento_Customer/js/customer-data'], (customerData) => {
+  document.addEventListener('cartDataChanged', (e) => {
+    console.log('Cart data:', e.detail);
+    const cartData = e.detail;
     const thisProductSampleSku = document.querySelector('.sample-add-form form').dataset.productSku;
-    const cart = customerData.get('cart'); //Get cart data
-    cart.subscribe((updatedCart) => {
-      console.log('update cart', updatedCart); //Handle updated cart data
-      //check if the current product sample is in the cart
-      const sampleInCart = updatedCart.items.find(
-        (item) => item.product_sku === thisProductSampleSku
-      );
+    const sampleInCart = cartData.items.find((item) => item.product_sku === thisProductSampleSku);
 
-      const currentSampleQty = sampleInCart ? sampleInCart.qty : 0;
+    const currentSampleQty = sampleInCart ? sampleInCart.qty : 0;
 
-      const totalSampleQty = updatedCart.items.reduce(
-        (acc, item) => (item.product_name.includes('Free Sample') ? acc + item.qty : acc),
-        0
-      );
+    const totalSampleQty = cartData.items.reduce(
+      (acc, item) => (item.product_name.includes('Free Sample') ? acc + item.qty : acc),
+      0
+    );
 
-      let btnSubtextMsg = '';
-      console.log('🚀 ~ cart.subscribe ~ currentSampleQty:', currentSampleQty);
+    let btnSubtextMsg = '';
+    //console.log('🚀 ~ cart.subscribe ~ currentSampleQty:', currentSampleQty);
 
-      if (currentSampleQty > 1) {
-        stage3Msg = limitReached;
-        finalMessage = limitReached;
-        btnSubtextMsg = "You've hit the sample limit for this product.";
-      } else {
-        stage3Msg = 'Added to basket';
-        finalMessage = 'Order a sample';
-      }
-      console.log('🚀 ~ cart.subscribe ~ totalSampleQty:', totalSampleQty);
+    if (currentSampleQty > 1) {
+      stage3Msg = limitReached;
+      finalMessage = limitReached;
+      btnSubtextMsg = "You've hit the sample limit for this product.";
+    } else {
+      stage3Msg = 'Added to basket';
+      finalMessage = 'Order a sample';
+    }
+    console.log('🚀 ~ cart.subscribe ~ totalSampleQty:', totalSampleQty);
 
-      if (totalSampleQty >= 15) {
-        finalMessage = limitReached;
-        stage3Msg = limitReached;
-        btnSubtextMsg = "You've hit the sample limit for this order.";
-      }
+    if (totalSampleQty >= 15) {
+      finalMessage = limitReached;
+      stage3Msg = limitReached;
+      btnSubtextMsg = "You've hit the sample limit for this order.";
+    }
 
-      //update btn text
-      const orderSampleCta = document.querySelector(`.${ID}__order-button span`);
-      const btnSubtext = document.querySelector(`.${ID}__order-button--subtext`);
-      if (orderSampleCta) {
-        orderSampleCta.innerText = finalMessage;
-      }
-      if (btnSubtext) {
-        btnSubtext.innerText = btnSubtextMsg;
-      }
-    });
+    //update btn text
+    const orderSampleCta = document.querySelector(`.${ID}__order-button span`);
+    const btnSubtext = document.querySelector(`.${ID}__order-button--subtext`);
+    if (orderSampleCta) {
+      orderSampleCta.innerText = finalMessage;
+    }
+    if (btnSubtext) {
+      btnSubtext.innerText = btnSubtextMsg;
+    }
   });
 
   if (!document.querySelector(`.${ID}__order-button`)) {
