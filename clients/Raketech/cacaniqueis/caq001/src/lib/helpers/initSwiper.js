8d38c9ef9d9c272cf4284a6a57554d6142849606
maaislam <maaislam@outlook.com>
2024-03-26 17:26:26 +0600

    caca casino test 1 initial buiild

---
+++ clients/Raketech/cacaniqueis/caq001/src/lib/helpers/initSwiper.js
@@ -0,0 +1,24 @@
+const initSwiper = (id) => {
+  const styleSheet = document.createElement('link');
+  styleSheet.rel = 'stylesheet';
+  styleSheet.href = 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css';
+  document.head.appendChild(styleSheet);
+
+  const script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js';
+  document.head.appendChild(script);
+
+  script.onload = () => {
+    const swiper = new Swiper(`.${id}__swiper`, {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      }
+    });
+  };
+};
+
+export default initSwiper;
---
+++ clients/Raketech/cacaniqueis/caq001/src/lib/shared/shared.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ID: 'caq001',
+  VARIATION: '1',
+  CLIENT: 'Raketech',
+  SITE: 'cacaniqueis'
+};
