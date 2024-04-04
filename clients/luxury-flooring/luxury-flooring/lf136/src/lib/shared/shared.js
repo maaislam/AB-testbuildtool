46bf8b7a11b67461612d2aff72925f3cf7e36cdd
maaislam <maaislam@outlook.com>
2024-04-05 05:31:20 +0600

    testing lint

--- clients/luxury-flooring/luxury-flooring/lf136/src/lib/shared/shared.js
+++ clients/luxury-flooring/luxury-flooring/lf136/src/lib/shared/shared.js
@@ -1,6 +1,6 @@
 module.exports = {
-    ID: "lf136",
-    VARIATION: "1",
-    CLIENT: "luxury-flooring",
-    SITE: "luxury-flooring"
-  };
+  ID: 'lf136',
+  VARIATION: '1',
+  CLIENT: 'luxury-flooring',
+  SITE: 'luxury-flooring'
+};
