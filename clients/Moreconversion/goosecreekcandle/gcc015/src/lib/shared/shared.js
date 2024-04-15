b807c2dc32636be7724de8be03a717aa03788d2e
raihankabir36850 <raihanku320@gmail.com>
2024-04-15 23:48:14 +0600

    css changes in test gcc015

--- clients/Moreconversion/goosecreekcandle/gcc015/src/lib/shared/shared.js
+++ clients/Moreconversion/goosecreekcandle/gcc015/src/lib/shared/shared.js
@@ -1,6 +1,6 @@
 module.exports = {
-  ID: 'gcc015',
-  VARIATION: '1',
-  CLIENT: 'Moreconversion',
-  SITE: 'goosecreekcandle'
-};
+    ID: "gcc015",
+    VARIATION: "1",
+    CLIENT: "Moreconversion",
+    SITE: "goosecreekcandle"
+  };
