47708e660d3af0484b0759237ec03b65c504813f
SakibAlIslam <sakibalislam12345@gmail.com>
2024-04-22 16:06:54 +0600

    test

--- clients/Moreconversion/hoodies/HOD001/src/index.js
+++ clients/Moreconversion/hoodies/HOD001/src/index.js
@@ -1,4 +1,4 @@
 import activate from './lib/experiment';
 import { pollerLite } from './lib/helpers/utils';
 
-pollerLite(['.product__info-wrapper variant-radios'], activate);
+//pollerLite(['.product__info-wrapper variant-radios'], activate);
