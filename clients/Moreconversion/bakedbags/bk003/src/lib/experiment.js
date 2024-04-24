d45fd71dd936c1e162ae214f22abde441a36451e
maaislam <maaislam@outlook.com>
2024-04-24 20:55:59 +0600

    css update

--- clients/Moreconversion/bakedbags/bk003/src/lib/experiment.js
+++ clients/Moreconversion/bakedbags/bk003/src/lib/experiment.js
@@ -3,7 +3,7 @@ import setup from './services/setup';
 import shared from './shared/shared';
 import { pollerLite } from './helpers/utils';
 
-const { ID, VARIATION } = shared;
+const { ID } = shared;
 
 const init = () => {
   const targetPoint = document.querySelector(
--- clients/Moreconversion/bakedbags/bk003/src/lib/shared/shared.js
+++ clients/Moreconversion/bakedbags/bk003/src/lib/shared/shared.js
@@ -1,6 +1,6 @@
 module.exports = {
-    ID: "bk003",
-    VARIATION: "1",
-    CLIENT: "Moreconversion",
-    SITE: "bakedbags"
+    ID: 'bk003',
+    VARIATION: '1',
+    CLIENT: 'Moreconversion',
+    SITE: 'bakedbags'
   };
