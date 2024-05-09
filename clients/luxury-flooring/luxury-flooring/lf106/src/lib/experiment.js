5972750fd7f97c8b1bf092cd4293ecbc234dbaeb
raihankabir36850 <raihanku320@gmail.com>
2024-05-09 23:43:27 +0600

    create a new test of luxury-flooring

---
+++ clients/luxury-flooring/luxury-flooring/lf106/src/lib/experiment.js
@@ -0,0 +1,23 @@
+import setup from './services/setup';
+import shared from './shared/shared';
+import { trustpilot } from './components/trustpilot';
+
+const { ID, VARIATION } = shared;
+
+const init = () => {
+  const targetElement = document.querySelector('.usps-main');
+  if (document.querySelector(`.${ID}__trustpilotSection`)) {
+    document.querySelector(`.${ID}__trustpilotSection`).remove();
+  }
+  targetElement && targetElement.insertAdjacentHTML('beforebegin', trustpilot(ID));
+};
+
+export default () => {
+  setup(); //use if needed
+  console.log(ID);
+
+  //eslint-disable-next-line no-useless-return
+  if (VARIATION === 'Control') return;
+
+  init();
+};
