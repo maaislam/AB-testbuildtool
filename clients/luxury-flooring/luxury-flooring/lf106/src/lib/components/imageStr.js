3b242b3cf8dc47109bbe85ca99e649085fae000a
raihankabir36850 <raihanku320@gmail.com>
2024-05-13 20:57:24 +0600

    svg updated in test lf106

--- clients/luxury-flooring/luxury-flooring/lf106/src/lib/components/imageStr.js
+++ clients/luxury-flooring/luxury-flooring/lf106/src/lib/components/imageStr.js
@@ -1,10 +1,10 @@
-import { affordableFloors, luxuryService } from '../assets/icons';
+import { desktopIcon, mobileIcon } from '../assets/icons';
 
 export const imageStr = (id) => {
   const html = `
         <div class="${id}__images">
-            <div class="${id}__images-item">${affordableFloors}</div>
-            <div class="${id}__images-item">${luxuryService}</div>
+            <div class="${id}__images-item ${id}__desktopIcon">${desktopIcon}</div>
+            <div class="${id}__images-item ${id}__mobileIcon">${mobileIcon}</div>
         </div>
     `;
   return html;
