2f8d7c5d900f50f63a2a83312ab40f1fd3a916f3
SakibAlIslam <sakibalislam12345@gmail.com>
2024-05-21 14:58:32 +0600

    This commit improves the lazy loading functionality for images by updating the activation logic in the index.js file. Instead of using a class selector, it now uses an ID selector to target the cart popup element. Additionally, a setTimeout function is added to delay the activation by 1500 milliseconds.

--- clients/Moreconversion/angelicmotion/agm006/src/index.js
+++ clients/Moreconversion/angelicmotion/agm006/src/index.js
@@ -1,4 +1,4 @@
 import activate from './lib/experiment';
 import { pollerLite } from './lib/helpers/utils';
 
-pollerLite(['.upcart-upsells-title'], activate);
+pollerLite(['#CartPopup'], setTimeout(activate, 1500));
