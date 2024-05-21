ef9ea641342787c1ce68167425a7c2d89037374f
SakibAlIslam <sakibalislam12345@gmail.com>
2024-05-21 15:29:42 +0600

    update lf new bug and amends

--- clients/luxury-flooring/luxury-flooring/lf167/src/lib/components/tradePrice.js
+++ clients/luxury-flooring/luxury-flooring/lf167/src/lib/components/tradePrice.js
@@ -4,7 +4,7 @@ export const tradePrice = (id) => {
   const html = `
         <div class="${id}__tradePrice">
             <div class="${id}__tradePrice-icon">${tradePriceIcon}</div>
-            <a class="${id}__tradePrice-link">Get trade price</a>
+            <a href='https://www.luxuryflooringandfurnishings.co.uk/trade-discounts' class="${id}__tradePrice-link">Get trade price</a>
         </div>
     `;
   return html;
