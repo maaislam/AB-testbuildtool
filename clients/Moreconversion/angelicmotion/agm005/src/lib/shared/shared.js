bf205d04cf73b7c5da7ad2025cd48491658d2dee
maaislam <maaislam@outlook.com>
2024-05-28 14:31:29 +0600

    lf167 updates

--- clients/Moreconversion/angelicmotion/agm005/src/lib/shared/shared.js
+++ clients/Moreconversion/angelicmotion/agm005/src/lib/shared/shared.js
@@ -1,6 +1,6 @@
 module.exports = {
-    ID: "agm005",
-    VARIATION: "1",
-    CLIENT: "Moreconversion",
-    SITE: "angelicmotion"
-  };
+  ID: 'agm005',
+  VARIATION: '1',
+  CLIENT: 'Moreconversion',
+  SITE: 'angelicmotion'
+};
