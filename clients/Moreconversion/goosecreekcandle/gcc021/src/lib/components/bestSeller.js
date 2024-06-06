b46b9793c63793128e1689d4c9270e6e71669250
raihankabir36850 <raihanku320@gmail.com>
2024-06-07 01:53:54 +0600

    test 3 test updated

--- clients/Moreconversion/goosecreekcandle/gcc021/src/lib/components/bestSeller.js
+++ clients/Moreconversion/goosecreekcandle/gcc021/src/lib/components/bestSeller.js
@@ -1,7 +1,7 @@
 export const bestSeller = (id, type) => {
   const htmlDesktop = `
         <li class="dropdown-submenu ${id}__menu-link ${id}__${type}">
-            <a href="#" aria-expanded="false">
+            <a href="/collections/all-products" aria-expanded="false">
                 Best Sellers           
             </a>        
         </li>
@@ -9,7 +9,7 @@ export const bestSeller = (id, type) => {
 
   const htmlMobile = `
         <li class="${id}__menu-link ${id}__${type}">   
-            <a class="alt-focus mobile-menu-link mobile-menu-link__text type-subheading" href="#">
+            <a class="alt-focus mobile-menu-link mobile-menu-link__text type-subheading" href="/collections/all-products">
                 Best Sellers
             </a>      
         </li>
