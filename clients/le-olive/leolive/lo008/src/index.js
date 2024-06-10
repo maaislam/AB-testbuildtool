8845a6a8715a1f4ae016739fcbf474c7a5277337
raihankabir36850 <raihanku320@gmail.com>
2024-06-11 01:14:37 +0600

    test added lo008

---
+++ clients/le-olive/leolive/lo008/src/index.js
@@ -0,0 +1,4 @@
+import activate from './lib/experiment';
+import { pollerLite } from './lib/helpers/utils';
+
+pollerLite(['body.template-product', 'form[id^="product_form"]'], activate);
---
+++ clients/le-olive/leolive/lo008/src/lib/data/data.js
@@ -0,0 +1,535 @@
+const colorObj = {
+  classic: [
+    {
+      name: 'powder pink',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/powder-pink.png',
+      productUrl: '/collections/fleece-badjassen/products/fleece-badjas-poeder-roze'
+    },
+    {
+      name: 'taupe',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/black.png',
+      productUrl: '/collections/fleece-badjassen/products/fleece-badjas-taupe'
+    },
+    {
+      name: 'cream pearl',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/cream-pearl.png',
+      productUrl: '/collections/fleece-badjassen/products/robe-beige-white-piping'
+    },
+    {
+      name: 'sand',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/black.png',
+      productUrl: '/collections/fleece-badjassen/products/badjas-sand'
+    },
+    {
+      name: 'stone pearl',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/stone-pearl.png',
+      productUrl: '/collections/fleece-badjassen/products/badjas-double-piping'
+    },
+    {
+      name: 'black',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/black.png',
+      productUrl: '/collections/fleece-badjassen/products/fleece-badjas-zwart'
+    },
+    {
+      name: 'white',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/white.png',
+      productUrl: '/collections/fleece-badjassen/products/fleece-badjas-wit'
+    },
+    {
+      name: 'navy',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/black.png',
+      productUrl: '/collections/fleece-badjassen/products/fleece-badjas-donkerblauw'
+    },
+    {
+      name: 'brown',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/brown.png',
+      productUrl: '/collections/fleece-badjassen/products/badjas-chocolate-brown'
+    },
+    {
+      name: 'light pink',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/light-pink.png',
+      productUrl: '/collections/fleece-badjassen/products/badjas-light-pink'
+    },
+    {
+      name: 'lilac',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/black.png',
+      productUrl: '/collections/fleece-badjassen/products/robe-lilac'
+    },
+    {
+      name: 'baby blue',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/black.png',
+      productUrl: '/collections/fleece-badjassen/products/robe-baby-blue'
+    },
+    {
+      name: 'leopard',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/leopard.png',
+      productUrl: '/collections/fleece-badjassen/products/badjas-luipaard'
+    }
+  ],
+  waffle: [
+    {
+      name: 'powder pink',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/powder-pink.png',
+      productUrl: '/collections/fleece-badjassen/products/waffle-badjas-powder-pink'
+    },
+    {
+      name: 'taupe',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/waffle-badjas-taupe'
+    },
+    {
+      name: 'white',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/white.png',
+      productUrl: '/collections/fleece-badjassen/products/waffle-badjas-white'
+    }
+  ],
+  long: [
+    {
+      name: 'red',
+      imageUrl: 'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/red.png',
+      productUrl: '/collections/fleece-badjassen/products/badjas-bordeaux-red'
+    },
+    {
+      name: 'emerald green',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/emerald.png',
+      productUrl: '/collections/fleece-badjassen/products/badjas-emerald-green'
+    },
+    {
+      name: 'black',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/kimono-deluxe-long-midnight-black'
+    },
+    {
+      name: 'cream',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/kopie-van-kimono-deluxe-long-cream'
+    },
+    {
+      name: 'white',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/white.png',
+      productUrl: '/collections/fleece-badjassen/products/kimono-deluxe-long-white'
+    }
+  ],
+  velours: [
+    {
+      name: 'champagne',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/kimono-velours-champagne'
+    },
+    {
+      name: 'powder pink',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/powder-pink.png',
+      productUrl: '/collections/fleece-badjassen/products/kimono-velours-powder-pink'
+    },
+    {
+      name: 'brown',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/brown.png',
+      productUrl: '/collections/fleece-badjassen/products/kimono-velours-brown'
+    },
+    {
+      name: 'black',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/kimono-velours-midnight-black'
+    },
+    {
+      name: 'grey',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/kimono-velours-grey'
+    }
+  ],
+  cotton: [
+    {
+      name: 'powder pink',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/powder-pink.png',
+      productUrl: '/collections/fleece-badjassen/products/badjas-katoen-powder-pink'
+    },
+    {
+      name: 'taupe',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/badjas-katoen-taupe'
+    },
+    {
+      name: 'black',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/badjas-katoen-midnight-black'
+    }
+  ],
+  hooded: [
+    {
+      name: 'pink',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/badjas-pink-hooded'
+    },
+    {
+      name: 'black',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/badjas-black-hooded'
+    },
+    {
+      name: 'orchid pink',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/badjas-hooded-orchid-pink'
+    }
+  ],
+  deluxe: [
+    {
+      name: 'cuban sand',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/badjas-cuban-sand'
+    },
+    {
+      name: 'orchid pink',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/badjas-orchid-pink'
+    },
+    {
+      name: 'frosty mint',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/badjas-mint-green'
+    }
+  ],
+  velvet: [
+    {
+      name: 'champagne',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/kimono-velvet-dark-grey'
+    },
+    {
+      name: 'brown',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/brown.png',
+      productUrl: '/collections/fleece-badjassen/products/kimono-velvet-brown'
+    },
+    {
+      name: 'plum',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/kimono-velvet-grey'
+    },
+    {
+      name: 'emerald green',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/emerald.png',
+      productUrl: '/collections/fleece-badjassen/products/kimono-velvet-green-long'
+    },
+    {
+      name: 'red',
+      imageUrl: 'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/red.png',
+      productUrl: '/collections/fleece-badjassen/products/kimono-velvet-red-long'
+    },
+    {
+      name: 'black',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/kimono-velvet-midnight-black'
+    },
+    {
+      name: 'blue',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/blue.png',
+      productUrl: '/collections/fleece-badjassen/products/kimono-velvet-cobalt-blue'
+    }
+  ],
+  teddy: [
+    {
+      name: 'champagne',
+      imageUrl: '',
+      productUrl: ''
+    },
+    {
+      name: 'black',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/kimono-teddy-midnightblack'
+    },
+    {
+      name: 'pink',
+      imageUrl: '',
+      productUrl: '/collections/fleece-badjassen/products/kimono-teddy-blossompink'
+    },
+    {
+      name: 'brown',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/brown.png',
+      productUrl: '/collections/fleece-badjassen/products/kimono-teddy-chocolate-brown'
+    }
+  ],
+  kids: [
+    {
+      name: 'powder pink',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/powder-pink.png',
+      productUrl: ''
+    },
+    {
+      name: 'taupe',
+      imageUrl: '',
+      productUrl: ''
+    },
+    {
+      name: 'light pink',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/light-pink.png',
+      productUrl: ''
+    },
+    {
+      name: 'cream pearl',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/cream-pearl.png',
+      productUrl: ''
+    },
+    {
+      name: 'navy',
+      imageUrl: '',
+      productUrl: ''
+    },
+    {
+      name: 'black',
+      imageUrl: '',
+      productUrl: ''
+    }
+  ],
+  baby: [
+    {
+      name: 'powder pink',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/powder-pink.png',
+      productUrl: ''
+    },
+    {
+      name: 'taupe',
+      imageUrl: '',
+      productUrl: ''
+    },
+    {
+      name: 'light pink',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/light-pink.png',
+      productUrl: ''
+    },
+    {
+      name: 'cream pearl',
+      imageUrl:
+        'https://abtest-luxuryflooring-salyenz.s3.us-west-2.amazonaws.com/le-olive/cream-pearl.png',
+      productUrl: ''
+    },
+    {
+      name: 'baby blue',
+      imageUrl: '',
+      productUrl: ''
+    }
+  ]
+};
+
+export const robesProductData = {
+  'fleece-badjas-poeder-roze': {
+    colorArray: colorObj.classic,
+    perSelectedColor: 'powder pink'
+  },
+  'fleece-badjas-taupe': {
+    colorArray: colorObj.classic,
+    perSelectedColor: 'taupe'
+  },
+  'robe-beige-white-piping': {
+    colorArray: colorObj.classic,
+    perSelectedColor: 'cream pearl'
+  },
+  'badjas-sand': {
+    colorArray: colorObj.classic,
+    perSelectedColor: ''
+  },
+  'badjas-double-piping': {
+    colorArray: colorObj.classic,
+    perSelectedColor: 'stone pearl'
+  },
+  'badjas-chocolate-brown': {
+    colorArray: colorObj.classic,
+    perSelectedColor: 'brown'
+  },
+  'fleece-badjas-wit': {
+    colorArray: colorObj.classic,
+    perSelectedColor: 'white'
+  },
+  'fleece-badjas-donkerblauw': {
+    colorArray: colorObj.classic,
+    perSelectedColor: 'navy'
+  },
+  'fleece-badjas-zwart': {
+    colorArray: colorObj.classic,
+    perSelectedColor: 'black'
+  },
+  'badjas-light-pink': {
+    colorArray: colorObj.classic,
+    perSelectedColor: 'light pink'
+  },
+  'robe-lilac': {
+    colorArray: colorObj.classic,
+    perSelectedColor: 'lilac'
+  },
+  'robe-baby-blue': {
+    colorArray: colorObj.classic,
+    perSelectedColor: 'baby blue'
+  },
+  'badjas-luipaard': {
+    colorArray: colorObj.classic,
+    perSelectedColor: 'leopard'
+  },
+  //teddy
+  'kimono-teddy-caramel': {
+    colorArray: colorObj.teddy,
+    //could not exact match
+    perSelectedColor: ''
+  },
+  'kimono-teddy-midnightblack': {
+    colorArray: colorObj.teddy,
+    perSelectedColor: 'black'
+  },
+  'kimono-teddy-blossompink': {
+    colorArray: colorObj.teddy,
+    perSelectedColor: 'blossom pink'
+  },
+  'kimono-teddy-chocolate-brown': {
+    colorArray: colorObj.teddy,
+    perSelectedColor: 'chocolate brown'
+  },
+  //deluxe
+  'badjas-cuban-sand': {
+    colorArray: colorObj.deluxe,
+    perSelectedColor: 'cuban sand'
+  },
+  'badjas-orchid-pink': {
+    colorArray: colorObj.deluxe,
+    perSelectedColor: 'orchid-pink'
+  },
+  'badjas-mint-green': {
+    colorArray: colorObj.deluxe,
+    perSelectedColor: 'frosty mint'
+  },
+  //long
+  'badjas-bordeaux-red': {
+    colorArray: colorObj.long,
+    perSelectedColor: ''
+  },
+  'badjas-emerald-green': {
+    colorArray: colorObj.long,
+    perSelectedColor: 'emerald green'
+  },
+  'kimono-deluxe-long-midnight-black': {
+    colorArray: colorObj.long,
+    perSelectedColor: ''
+  },
+  'kopie-van-kimono-deluxe-long-cream': {
+    colorArray: colorObj.long,
+    perSelectedColor: ''
+  },
+  'kimono-deluxe-long-white': {
+    colorArray: colorObj.long,
+    perSelectedColor: 'white'
+  },
+  //hooded
+  'badjas-hooded-orchid-pink': {
+    colorArray: colorObj.hooded,
+    perSelectedColor: 'orchid pink'
+  },
+  'badjas-pink-hooded': {
+    colorArray: colorObj.hooded,
+    perSelectedColor: 'pink'
+  },
+  'badjas-black-hooded': {
+    colorArray: colorObj.hooded,
+    perSelectedColor: 'black'
+  },
+  //waffle
+  'waffle-badjas-powder-pink': {
+    colorArray: colorObj.waffle,
+    perSelectedColor: 'powder pink'
+  },
+  'waffle-badjas-taupe': {
+    colorArray: colorObj.waffle,
+    perSelectedColor: 'taupe'
+  },
+  'waffle-badjas-white': {
+    colorArray: colorObj.waffle,
+    perSelectedColor: 'white'
+  },
+  //velours
+  'kimono-velours-powder-pink': {
+    colorArray: colorObj.velours,
+    perSelectedColor: 'powder pink'
+  },
+  'kimono-velours-brown': {
+    colorArray: colorObj.velours,
+    perSelectedColor: 'brown'
+  },
+  'kimono-velours-midnight-black': {
+    colorArray: colorObj.velours,
+    perSelectedColor: 'black'
+  },
+  'kimono-velours-champagne': {
+    colorArray: colorObj.velours,
+    perSelectedColor: 'champagne'
+  },
+  'kimono-velours-grey': {
+    colorArray: colorObj.velours,
+    perSelectedColor: 'grey'
+  },
+  //velvet
+  'kimono-velvet-red-long': {
+    colorArray: colorObj.velvet,
+    perSelectedColor: 'red'
+  },
+  'kimono-velvet-cobalt-blue': {
+    colorArray: colorObj.velvet,
+    perSelectedColor: 'blue'
+  },
+  'kimono-velvet-midnight-black': {
+    colorArray: colorObj.velvet,
+    perSelectedColor: 'black'
+  },
+  'kimono-velvet-green-long': {
+    colorArray: colorObj.velvet,
+    perSelectedColor: 'emerald green'
+  },
+  'kimono-velvet-dark-grey': {
+    colorArray: colorObj.velvet,
+    perSelectedColor: 'grey'
+  },
+  'kimono-velvet-brown': {
+    colorArray: colorObj.velvet,
+    perSelectedColor: 'brown'
+  },
+  'kimono-velvet-grey': {
+    colorArray: colorObj.velvet,
+    perSelectedColor: 'plum'
+  },
+  //cotton
+  'badjas-katoen-powder-pink': {
+    colorArray: colorObj.cotton,
+    perSelectedColor: 'powder pink'
+  },
+  'badjas-katoen-taupe': {
+    colorArray: colorObj.cotton,
+    perSelectedColor: 'taupe'
+  },
+  'badjas-katoen-midnight-black': {
+    colorArray: colorObj.cotton,
+    perSelectedColor: 'black'
+  }
+};
