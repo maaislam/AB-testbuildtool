e2ce57469c09c803c8e1b05e95fa8cf298873557
raihankabir36850 <raihanku320@gmail.com>
2024-06-14 19:25:06 +0600

    desktop review section ready

--- clients/luxury-flooring/luxury-flooring/lf169/src/lib/experiment.js
+++ clients/luxury-flooring/luxury-flooring/lf169/src/lib/experiment.js
@@ -18,5 +18,30 @@ export default () => {
 
   console.log('id', ID);
 
+  document.body.addEventListener('click', ({ target }) => {
+    if (
+      target.closest(`.${ID}__readMoreReviews`) &&
+      target.closest(`.${ID}__readMoreReviews`).dataset.initial === 'true'
+    ) {
+      const listContainer = target.closest('.pdp-reviews-container');
+      const actualHeight = listContainer.querySelector('.pdp-reviews-list-container').dataset
+        .height;
+      const hiddenReviews = document.querySelectorAll(`.pdp-review.${ID}__hide`);
+      hiddenReviews.forEach((item) => {
+        item.classList.remove(`${ID}__hide`);
+      });
+
+      listContainer.querySelector('.pdp-reviews-list-container').style.height = `${actualHeight}`;
+      //eslint-disable-next-line no-param-reassign
+      target.closest(`.${ID}__readMoreReviews`).dataset.initial = 'false';
+    } else if (
+      target.closest(`.${ID}__readMoreReviews`) &&
+      target.closest(`.${ID}__readMoreReviews`).dataset.initial === 'false'
+    ) {
+      const reviewList = target.closest('.pdp-reviews-list');
+      reviewList.querySelector('button').click();
+    }
+  });
+
   init();
 };
