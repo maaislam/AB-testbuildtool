1ce99724e29b909028bb037d50c62336201e01d9
SakibAlIslam <sakibalislam12345@gmail.com>
2024-07-05 23:23:03 +0600

    kt013 removed

--- clients/Moreconversion/kittyspout/ks013/src/index.js
+++
@@ -1,4 +0,0 @@
-import activate from './lib/experiment';
-import { pollerLite } from './lib/helpers/utils';
-
-pollerLite(['body.template-product', '.product-slideshow.flickity-enabled'], activate);
--- clients/Moreconversion/kittyspout/ks013/src/lib/shared/shared.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    ID: "ks013",
-    VARIATION: "1",
-    CLIENT: "Moreconversion",
-    SITE: "kittyspout"
-  };
