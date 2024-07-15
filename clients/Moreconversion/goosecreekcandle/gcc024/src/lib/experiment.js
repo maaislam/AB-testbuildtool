7d607120c8f860605c1ca96c68b7767e95f8642e
raihankabir36850 <raihanku320@gmail.com>
2024-07-15 15:24:26 +0600

    done gc024 test

--- clients/Moreconversion/goosecreekcandle/gcc024/src/lib/experiment.js
+++ clients/Moreconversion/goosecreekcandle/gcc024/src/lib/experiment.js
@@ -2,16 +2,20 @@
 import setup from './services/setup';
 import gaTracking from './services/gaTracking';
 import shared from './shared/shared';
-import { obsIntersection } from './helpers/utils';
+import { obsIntersection, observeDOM } from './helpers/utils';
 
 const { ID, VARIATION } = shared;
 //eslint-disable-next-line prefer-const
 let pageCounter = 0;
 
 const init = () => {
-  const paginationLastItem = document.querySelector('.pagination').lastElementChild;
-  const nextButtonElem = paginationLastItem.querySelector('a');
-  const nextButtonUrl = nextButtonElem.href;
+  const paginationLastItem = document.querySelector('.pagination')?.lastElementChild;
+  const nextButtonElem = paginationLastItem?.querySelector('a');
+  const nextButtonUrl = nextButtonElem?.href;
+
+  if (!nextButtonUrl) {
+    return;
+  }
 
   const url = new URL(nextButtonUrl);
   url.searchParams.set('page', pageCounter);
@@ -22,12 +26,15 @@ const init = () => {
       const parser = new DOMParser();
       const doc = parser.parseFromString(html, 'text/html');
       const collection = doc.querySelectorAll('#template--collection li');
-      const currentPageList = document.querySelector('#template--collection');
-      collection.forEach((item) => {
-        currentPageList.insertAdjacentElement('beforeend', item);
-      });
+      if (collection.length > 0) {
+        const currentPageList = document.querySelector('#template--collection');
 
-      pageCounter += 1;
+        collection.forEach((item) => {
+          currentPageList.insertAdjacentElement('beforeend', item);
+        });
+
+        pageCounter += 1;
+      }
     })
     .catch((error) => {
       console.log('An error occurred:', error);
@@ -36,15 +43,19 @@ const init = () => {
 
 export default () => {
   setup();
-  console.log(ID);
 
   if (VARIATION === 'control') return;
 
-  const listItems = document.querySelectorAll('.pagination .page-item');
-  const listArray = Array.from(listItems);
-  const activeIndex = listArray.findIndex((item) => item.classList.contains('active'));
+  const getActiveFn = () => {
+    const listItems = document.querySelectorAll('.pagination .page-item');
+    const listArray = Array.from(listItems);
+    const activeIndex = listArray?.findIndex((item) => item.classList.contains('active'));
+    if (activeIndex) {
+      pageCounter = activeIndex + 1;
+    }
+  };
 
-  pageCounter = activeIndex;
+  getActiveFn();
 
   const obserElem = document.querySelector('.collection-bottom-description > span');
   const callBackHandler = (entry) => {
@@ -53,4 +64,9 @@ export default () => {
     }
   };
   obsIntersection(obserElem, 1, callBackHandler);
+
+  observeDOM('#CollectionProductGrid', (mutation) => {
+    pageCounter = 0;
+    getActiveFn();
+  });
 };
