46158f5490f9b2021042fd7d086c968e34e9639f
SakibAlIslam <sakibalislam12345@gmail.com>
2024-07-29 16:29:35 +0600

    image slider issue fixed

--- clients/luxury-flooring/luxury-flooring/lf306/src/lib/experiment.js
+++ clients/luxury-flooring/luxury-flooring/lf306/src/lib/experiment.js
@@ -10,7 +10,30 @@ const init = () => {
   if (!document.querySelector(`.${ID}__banner`) || !document.querySelector(`.${ID}__bannerV2`)) {
     attachPoint.insertAdjacentHTML('beforebegin', banner(ID, VARIATION));
 
-    if (VARIATION === '1') return;
+    //add fade in-out effect to the slider images for V1
+    if (VARIATION === '1') {
+      const beforeSlides = document.querySelectorAll('.before-slide');
+      const afterSlides = document.querySelectorAll('.after-slide');
+
+      let currentSlideBefore = 0;
+      let currentSlideAfter = 0;
+      const slideInterval = 5000; //5 seconds
+
+      const showNextSlide = () => {
+        beforeSlides[currentSlideBefore].classList.remove('active');
+        afterSlides[currentSlideAfter].classList.remove('active');
+
+        currentSlideBefore = (currentSlideBefore + 1) % beforeSlides.length;
+        currentSlideAfter = (currentSlideAfter + 1) % afterSlides.length;
+
+        beforeSlides[currentSlideBefore].classList.add('active');
+        afterSlides[currentSlideAfter].classList.add('active');
+      };
+
+      setInterval(showNextSlide, slideInterval);
+
+      return;
+    }
 
     const container = document.querySelector(`.${ID}__banner-container`);
     const slider = document.querySelector(`.${ID}__slider`);
--- clients/luxury-flooring/luxury-flooring/lf306/src/lib/shared/shared.js
+++ clients/luxury-flooring/luxury-flooring/lf306/src/lib/shared/shared.js
@@ -1,6 +1,6 @@
 module.exports = {
     ID: "lf306",
-    VARIATION: "2",
+    VARIATION: "1",
     CLIENT: "luxury-flooring",
     SITE: "luxury-flooring"
   };
