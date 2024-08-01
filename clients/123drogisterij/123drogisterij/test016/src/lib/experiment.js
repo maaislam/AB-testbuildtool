31ec53d7d9d582112914c51630163b59082c1fbb
SakibAlIslam <sakibalislam12345@gmail.com>
2024-08-01 23:02:31 +0600

    123 - Update font sizes and add text message in experiment.scss

--- clients/123drogisterij/123drogisterij/test016/src/lib/experiment.js
+++ clients/123drogisterij/123drogisterij/test016/src/lib/experiment.js
@@ -1,11 +1,16 @@
+/*eslint-disable max-len */
 import initialProductsFetch from './helpers/initialProductsFetch';
 import setup from './services/setup';
 import shared from './shared/shared';
 
-const { ID, VARIATION } = shared;
+const { ID } = shared;
 
 const init = () => {
   const cartItems = document.querySelectorAll('.cart.item .item-info');
+
+  const textMsgHtml = (nextPossibleUpgrades) => {
+    return `<div class="${ID}__textMsg" data-nextpossibleupgrades="${nextPossibleUpgrades}">Kies ${nextPossibleUpgrades} stuks en krijg extra korting</div>`;
+  };
 
   const fetchPromises = Array.from(cartItems).map((cartItem) => {
     cartItem.classList.add(`${ID}__cartItem`);
@@ -18,10 +23,36 @@ const init = () => {
       docs.forEach((doc, index) => {
         const upsellOptions = doc.querySelectorAll('.custom-child-upsel-checkbox input');
         const rowItem = document.querySelectorAll(`.${ID}__cartItem`)[index];
+        const qtyInputElem = rowItem.querySelector('.input-text.qty');
+        const prdItemDetailsElem = rowItem.querySelector(`.${ID}__cartItem .product-item-details`);
+        const qtyValue = qtyInputElem.value;
+        const options = [];
+
         upsellOptions.forEach((upsellOption, upsellIndex) => {
           const inputValue = upsellOption.value;
-          if (inputValue > 1) rowItem.setAttribute(`data-bulkby-option-${upsellIndex + 1}`, inputValue);
+          if (inputValue > 1) {
+            rowItem.setAttribute(`data-bulkby-option-${upsellIndex + 1}`, inputValue);
+            options.push(inputValue);
+          }
         });
+
+        const possibleUpgrades = options.reduce((acc, option) => {
+          if (Number(option) > qtyValue) {
+            acc.push(Number(option));
+          }
+          return acc;
+        }, []);
+
+        const nextPossibleUpgrades = possibleUpgrades && possibleUpgrades.length > 0 ? Math.min(...possibleUpgrades) : null;
+
+        if (nextPossibleUpgrades && !rowItem.querySelector(`.${ID}__textMsg`)) {
+          prdItemDetailsElem.insertAdjacentHTML('beforeend', textMsgHtml(nextPossibleUpgrades));
+        }
+        // qtyInputElem.addEventListener('change', (e) => {
+        //   const { target } = e;
+        //   const qtyValue = target.value;
+        //   console.log('qtyValue: ', qtyValue);
+        // });
       });
     })
     .catch((error) => {
@@ -33,4 +64,19 @@ export default () => {
   setup(); //use if needed
 
   init();
+
+  document.body.addEventListener('click', (e) => {
+    const { target } = e;
+
+    if (target.closest(`.${ID}__textMsg`)) {
+      const updateCartElem = document.querySelector('.action.update');
+      const itemRow = target.closest(`.${ID}__cartItem`);
+      const qtyInputElem = itemRow.querySelector('.input-text.qty');
+      const nextPossibleUpgrade = target.closest(`.${ID}__textMsg`).dataset.nextpossibleupgrades;
+
+      qtyInputElem.value = Number(nextPossibleUpgrade);
+
+      updateCartElem.click();
+    }
+  });
 };
