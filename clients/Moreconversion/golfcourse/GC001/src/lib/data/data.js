20d16691cfd16ec6a06593350481b3d14f50ecba
SakibAlIslam <sakibalislam12345@gmail.com>
2024-08-17 03:16:37 +0600

    GC001 golf course project - created

---
+++ clients/Moreconversion/golfcourse/GC001/src/lib/data/data.js
@@ -0,0 +1,17 @@
+export const colorData = [
+    {
+        name: 'white',
+        color: '#FFF',
+        src: ''
+    },
+    {
+        name: 'black',
+        color: '#000',
+        src: ''
+    },
+    {
+        name: 'Natural Wood',
+        color: '',
+        src: 'https://cdn.shopify.com/s/files/1/0637/2701/0025/files/Natural-Wood.png?v=1723834020'
+    }
+];
