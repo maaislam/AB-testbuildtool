e80d6d6b9d107a24f38f1a9ce4168373d5819603
maaislam <maaislam@outlook.com>
2024-08-20 07:25:38 +0600

    gcc029 new build

--- clients/Moreconversion/goosecreekcandle/gcc024/src/lib/experiment.js
+++ clients/Moreconversion/goosecreekcandle/gcc024/src/lib/experiment.js
@@ -3,6 +3,7 @@ import setup from './services/setup';
 
 import shared from './shared/shared';
 import { obsIntersection, observeDOM, pollerLite } from './helpers/utils';
+import prodCard from './components/prodCard';
 
 const { ID, VARIATION } = shared;
 
@@ -15,10 +16,12 @@ const init = () => {
   const paginations = paginationParent.querySelectorAll('.boost-sd__pagination-number');
   const paginationLastItem = paginations[paginations.length - 1];
   const lastPagination = parseInt(paginationLastItem.textContent);
+  console.log('🚀 ~ init ~ lastPagination:', lastPagination);
   //const nextButtonElem = paginationLastItem?.querySelector('a');
   //const nextButtonUrl = nextButtonElem?.href;
 
-  if (lastPagination === pageCounter) {
+  if (lastPagination + 1 === pageCounter) {
+    console.log('🚀 no pages left');
     return;
   }
 
@@ -38,21 +41,25 @@ const init = () => {
   if (!document.querySelector(`.${ID}__loader`)) {
     currentPageList.insertAdjacentHTML('afterend', loadingElem);
   }
-  fetch(url)
-    .then((response) => response.text())
-    .then((html) => {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(html, 'text/html');
-      setTimeout(() => {
-        const collection = doc.querySelectorAll('.boost-sd__product-list > div');
-        collection.forEach((item) => {
-          currentPageList.insertAdjacentElement('beforeend', item);
-        });
 
-        pageCounter += 1;
-        const loaderElem = document.querySelector(`.${ID}__loader`);
-        loaderElem?.remove();
-      }, 5000);
+  const fetchUrl = `https://services.mybcapps.com/bc-sf-filter/filter?_=pf&shop=goose-creek-co.myshopify.com&page=${pageCounter}&limit=24&sort=manual&locale=en&event_type=init&pg=collection_page&build_filter_tree=true&collection_scope=129543700555&money_format=%26%2336%3B%7B%7Bamount%7D%7D&money_format_with_currency=%26%2336%3B%7B%7Bamount%7D%7D+USD&viewAs=grid--4&device=&first_load=false&productImageWidth=300&productPerRow=4&widget_updated_at=${Date.now()}&templateId=hWrzPXMYu4&isMobile=false&isTabletPortraitMax=false&behavior=refresh&tag=&t=${Date.now()}&sid=9f1bbba0-f15d-3974-2d7c-4baf3609519f&sort_first=available&product_available=false&variant_available=false`;
+  console.log('🚀 ~ init ~ pageCounter:', pageCounter);
+  fetch(fetchUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      //console.log('🚀 ~ .then ~ data:', data);
+
+      const { products } = data;
+
+      const insertElem = document.querySelector('.boost-sd__product-list');
+
+      products.forEach((product) => {
+        const prodHtml = prodCard(product);
+        insertElem.insertAdjacentHTML('beforeend', prodHtml);
+      });
+      pageCounter += 1;
+      const loaderElem = document.querySelector(`.${ID}__loader`);
+      loaderElem?.remove();
     })
     .catch((error) => {
       console.log('An error occurred:', error);
