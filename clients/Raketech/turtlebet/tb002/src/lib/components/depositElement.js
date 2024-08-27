9c1f894c0eb4a5c96bec4776539df06dd0cacbe2
raihankabir36850 <raihanku320@gmail.com>
2024-08-27 15:15:09 +0600

    done: new amends of turtlebet

--- clients/Raketech/turtlebet/tb002/src/lib/components/depositElement.js
+++ clients/Raketech/turtlebet/tb002/src/lib/components/depositElement.js
@@ -5,13 +5,15 @@ const zeroSpin = `
 `;
 
 const depositeElement = (id, element, min_deposit, sportsbook) => {
+  const regex = /(\d+)/;
+  const numberOnly = min_deposit.match(regex)[0];
   const html = `
         <div class="MuiGrid-root MuiGrid-container ${id}__depositeWrapper">
             ${element?.outerHTML || zeroSpin}
             ${bonusElement(id, sportsbook)}
             <div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-6 mui-1orysjz ${id}__depositeContainer">
                 <div class="mui-1ckvxhl">Minimitalletus</div>
-                <div class="mui-f14zgk ${id}__minimumDeposit">${min_deposit}</div>
+                <div class="mui-f14zgk ${id}__minimumDeposit">${numberOnly}€</div>
             </div>
         </div>
     `;
