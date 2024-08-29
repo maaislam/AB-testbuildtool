6b7413e6e32b46a2f2e08d16f02dcfe7c9cc0fc8
SakibAlIslam <sakibalislam12345@gmail.com>
2024-08-29 17:07:14 +0600

    Refactor index.js to remove unused code and improve performance

--- clients/123drogisterij/123drogisterij/test016/src/index.js
+++ clients/123drogisterij/123drogisterij/test016/src/index.js
@@ -1,29 +1,4 @@
 import activate from './lib/experiment';
 import { pollerLite } from './lib/helpers/utils';
 
-const getCookie = (name) => {
-    const value = `; ${document.cookie}`;
-
-    const parts = value.split(`; ${name}=`);
-
-    if (parts.length === 2) {
-        return parts.pop().split(';').shift();
-    }
-    return null;
-};
-
-const setCookie = (cName, cValue, expDays = 90) => {
-    const date = new Date();
-
-    date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
-
-    const expires = `expires=${date.toUTCString()}`;
-
-    document.cookie = `${cName}=${cValue}; ${expires}`;
-};
-
-window.setCookie = setCookie;
-
-if (getCookie('activate-049')) {
-    pollerLite([() => document.querySelectorAll('.cart.item .item-info').length > 0], activate);
-}
+pollerLite([() => document.querySelectorAll('.cart.item .item-info').length > 0], activate);
