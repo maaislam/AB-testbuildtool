b8db05b8b809917e119c2f620dfce99e97da2d04
raihankabir36850 <raihanku320@gmail.com>
2024-09-03 01:39:42 +0600

    lf171 test done and need to QA

--- clients/luxury-flooring/luxury-flooring/lf171/src/lib/components/calculateBox.js
+++ clients/luxury-flooring/luxury-flooring/lf171/src/lib/components/calculateBox.js
@@ -1,6 +1,17 @@
 const calculateBox = (id) => {
   const html = `
         <div class="${id}__calculateBox">
+            <div class="${id}__optionsWrapper">
+                <div class="${id}__options-container">
+                    <div class="${id}__options-price">Price your room</div>
+                    <div class="${id}__options-visualiser">
+                        <div class="${id}__options-icon">
+                            <img src="https://www.roomvo.com/services/vendor/vendor_images/luxuryflooringandfurnishingscouk/roomvo_icon.png">
+                        </div>
+                        <div class="${id}__options-text">Room Visualiser</div>
+                    </div>
+                </div>
+            </div>
             <form class="${id}__calculateBox-form">
                 <label for="fnumber">Price your room</label>
                 <div class="${id}__calculate-control">        
