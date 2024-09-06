7e2b9f2ced6276da2277ee5bba4f2eb4c6b76fda
SakibAlIslam <sakibalislam12345@gmail.com>
2024-09-06 18:55:34 +0600

    Variation 2 done

--- clients/Moreconversion/perfectwhitetee/pwt006/src/lib/experiment.js
+++ clients/Moreconversion/perfectwhitetee/pwt006/src/lib/experiment.js
@@ -12,49 +12,96 @@ const typeObj = {
 };
 
 const init = () => {
-  const collectionTitles = document.querySelectorAll('.grid-product__title');
-  pollerLite([() => collectionTitles.length > 0], () => {
-    const fetchPromises = [];
+  const { pathname } = window.location;
 
-    collectionTitles.forEach((collectionTitle) => {
-      const linkElem = collectionTitle.closest('.grid-product__link');
-      const link = linkElem.getAttribute('href');
+  if (pathname.includes('/collections/')) {
+    const collectionTitles = document.querySelectorAll('.grid-product__title');
+    pollerLite([() => collectionTitles.length > 0], () => {
+      const fetchPromises = [];
 
-      //const url = link.replace(/\?.*$/, '.js');
-      const urlObj = new URL(link, window.location.origin);
-      urlObj.search = ''; //Remove query parameters
-      const url = `${urlObj.href}.js`;
+      collectionTitles.forEach((collectionTitle) => {
+        const linkElem = collectionTitle.closest('.grid-product__link');
+        const link = linkElem.getAttribute('href');
 
-      fetchPromises.push(
-        fetch(url)
-          .then((response) => response.json())
-          .then((fileData) => ({ fileData, collectionTitle }))
-      );
-    });
+        //const url = link.replace(/\?.*$/, '.js');
+        const urlObj = new URL(link, window.location.origin);
+        urlObj.search = ''; //Remove query parameters
+        const url = `${urlObj.href}.js`;
 
-    Promise.all(fetchPromises)
-      .then((fileDataArray) => {
-        fileDataArray.forEach(({ fileData, collectionTitle }) => {
-          console.log('fileData: ', fileData);
-          const { type } = fileData;
-          const macthedPrdType = typeObj[type];
-          const [titleFirstPart, ...rest] = collectionTitle.textContent.trim().split(' ');
-          const productColor = rest.join(' ');
-          const prdType = type.toLowerCase();
-
-          console.log('productType: ', prdType);
-          console.log('productColor: ', productColor);
-
-          //Update the product title to include the product type
-          if (macthedPrdType || type) {
-            collectionTitle.textContent = `${titleFirstPart} ${macthedPrdType || prdType} ${productColor}`;
-          }
-        });
-      })
-      .catch((err) => {
-        console.log('Something went wrong with one or more fetch requests.', err);
+        fetchPromises.push(
+          fetch(url)
+            .then((response) => response.json())
+            .then((fileData) => ({ fileData, collectionTitle }))
+        );
       });
-  });
+
+      Promise.all(fetchPromises)
+        .then((fileDataArray) => {
+          fileDataArray.forEach(({ fileData, collectionTitle }) => {
+            console.log('fileData: ', fileData);
+            const { type } = fileData;
+            const macthedPrdType = typeObj[type];
+            const [titleFirstPart, ...rest] = collectionTitle.textContent.trim().split(' ');
+            const productColor = rest.join(' ');
+            const prdType = type.toLowerCase();
+
+            console.log('productType: ', prdType);
+            console.log('productColor: ', productColor);
+
+            //Update the product title to include the product type
+            if (macthedPrdType || type) {
+              collectionTitle.textContent = `${titleFirstPart} ${macthedPrdType || prdType} ${productColor}`;
+            }
+          });
+        })
+        .catch((err) => {
+          console.log('Something went wrong with one or more fetch requests.', err);
+        });
+    });
+  }
+
+  if (VARIATION === '2') {
+    //cart drawer
+    const cartTitles = document.querySelectorAll('#slidecarthq .item .title');
+    pollerLite([() => cartTitles.length > 0], () => {
+      const fetchPromises = [];
+
+      cartTitles.forEach((cartTitle) => {
+        const linkElem = cartTitle.querySelector('a');
+        const link = linkElem.getAttribute('href');
+
+        //const url = link.replace(/\?.*$/, '.js');
+        const urlObj = new URL(link, window.location.origin);
+        urlObj.search = ''; //Remove query parameters
+        const url = `${urlObj.href}.js`;
+
+        fetchPromises.push(
+          fetch(url)
+            .then((response) => response.json())
+            .then((fileData) => ({ fileData, cartTitle }))
+        );
+      });
+
+      Promise.all(fetchPromises)
+        .then((fileDataArray) => {
+          fileDataArray.forEach(({ fileData, cartTitle }) => {
+            const { type } = fileData;
+            const macthedPrdType = typeObj[type];
+            const [titleFirstPart, ...rest] = cartTitle.textContent.trim().split(' ');
+            const productColor = rest.join(' ');
+            const prdType = type.toLowerCase();
+
+            //Update the product title to include the product type
+            if (macthedPrdType || type) {
+              cartTitle.textContent = `${titleFirstPart} ${macthedPrdType || prdType} ${productColor}`;
+            }
+          });
+        })
+        .catch((err) => {
+          console.log('Something went wrong with one or more fetch requests.', err);
+        });
+    });
+  }
 };
 
 export default () => {
