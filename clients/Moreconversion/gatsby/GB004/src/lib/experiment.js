2bef1e1b6250d8f3344e9d1cf98cdf13112f16a5
SakibAlIslam <sakibalislam12345@gmail.com>
2024-09-06 16:49:50 +0600

    Update media query breakpoints in _variables.scss and experiment.scss

--- clients/Moreconversion/gatsby/GB004/src/lib/experiment.js
+++ clients/Moreconversion/gatsby/GB004/src/lib/experiment.js
@@ -6,28 +6,20 @@ import { freeShippingIcon } from './assets/svg';
 const { ID, VARIATION } = shared;
 
 const init = () => {
+  const attachPoint = document.body;
+
   if (VARIATION === '1') {
     pollerLite(['.essential_countdown_annoucement_bar_wrapper'], () => {
       const announcementBar = document.querySelector('.essential_countdown_annoucement_bar_wrapper');
-      //announcementBar.classList.add(`${ID}__announcementBar`);
-
-      const freeShippingHtml = `<div class='${ID}__freeShipping'>
-        <span>${freeShippingIcon}</span>
-        <div class='free-shipping-text'><b>Free Shipping</b> on all orders above $100</div>
-      </div>`;
-      if (!document.querySelector(`.${ID}__freeShipping`)) {
-        announcementBar.insertAdjacentHTML('afterbegin', freeShippingHtml);
-      }
+      announcementBar.classList.add(`${ID}__announcementBar`);
     }, 20000);
-
-    return;
   }
 
-  const attachPoint = document.body;
   const freeShippingHtml = `<div class='${ID}__freeShipping'>
         <span>${freeShippingIcon}</span>
         <div class='free-shipping-text'><b>Free Shipping</b> on all orders above $125</div>
       </div>`;
+
   if (!document.querySelector(`.${ID}__freeShipping`)) {
     attachPoint.insertAdjacentHTML('afterbegin', freeShippingHtml);
   }
