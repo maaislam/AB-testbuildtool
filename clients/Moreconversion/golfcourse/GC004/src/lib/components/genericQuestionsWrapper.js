e56607243b9cb1e1e10ee6e286c15925332a0a0e
raihankabir36850 <raihanku320@gmail.com>
2024-09-13 01:08:27 +0600

    golcourse test done

--- clients/Moreconversion/golfcourse/GC004/src/lib/components/genericQuestionsWrapper.js
+++ clients/Moreconversion/golfcourse/GC004/src/lib/components/genericQuestionsWrapper.js
@@ -1,72 +1,96 @@
-const genericQuestionsWrapper = (id) => {
+const genericQuestionsWrapper = (id, title) => {
   const html = `
-        <div class="${id}__container">
-            <h1>Frequently Asked Questions about Augusta National Golf Club, Georgia</h1>
-
-            <div class="faq">
-                <h2>Q: When will I receive my order?</h2>
-                <p>Due to demand of the holidays it may take 2-3 weeks to print and package your order, after which it’s shipped
-                    out. The shipping time depending on your location, is estimated as follows:</p>
-                <ul>
-                    <li>USA: 3-5 business days</li>
-                    <li>International: 10-20 business days</li>
-                </ul>
+        <div class="rte rte--nomargin ${id}__container">
+             <h1>Frequently Asked Questions about <span>${title}</span></h1>
+            <h2><span class="fontstyle2"><strong>When will I&nbsp;receive my order?</strong><br></span></h2>
+            <p><span class="fontstyle0">Due to demand of the holidays it may take 2-3 weeks&nbsp;to print and package your
+                    order,
+                    after which it’s shipped out. The shipping time depending&nbsp;on your location, is estimated as
+                    follows:<br></span><span class="fontstyle3">● </span><span class="fontstyle0">USA: 3–6 business
+                    days</span><span class="fontstyle0"><br></span><span class="fontstyle3">● </span><span
+                    class="fontstyle0">International: 10–20
+                    business days<br><br></span></p>
+            <h2><strong><span class="fontstyle2"></span></strong></h2>
+            <div class="collapse-heading pf-bg-white">
+                <h2><strong><span class="fontstyle0"></span></strong></h2>
+                <h2><strong><span class="fontstyle0">Will I be charged customs for my order?</span></strong></h2>
+                <p><span class="fontstyle2">An additional customs and tax fee can occur on international orders. This fee is not
+                        in
+                        our control and is assessed by your local customs office. Customs policies vary widely for every country
+                        so
+                        please check with your local customs office directly to see if they apply duties and taxes to your
+                        purchases.</span></p>
+                <h2>
+                    <span class="fontstyle2"><br></span><strong><span class="fontstyle0">My order should be here by now, but I
+                            still
+                            don't have it. What should I do?</span> </strong>
+                </h2>
+                <p><span class="fontstyle2">Before getting in touch with us, please help us out by doing the
+                        following:<br></span><span class="fontstyle3">● </span><span class="fontstyle2">Check your shipping
+                        confirmation email for any mistakes in the delivery address<br></span><span class="fontstyle3">●
+                    </span><span class="fontstyle2">Ask your local post office if they have your package<br></span><span
+                        class="fontstyle3">● </span><span class="fontstyle2">Stop by your neighbors in case the courier left
+                        the
+                        package with them.</span></p>
+                <p><span class="fontstyle2">If the shipping address was correct, and the package wasn't left at the post office
+                        or
+                        at your neighbor’s, get in touch with us with your order number by emailing
+                        hello@golfcourseprint.com</span><span class="fontstyle0"><br></span><span class="fontstyle0"></span></p>
+                <h2>
+                    <span class="fontstyle0"><strong>How are your products made?</strong><br></span><span
+                        class="fontstyle2"></span>
+                </h2>
+                <p><span class="fontstyle2">Every order is made to order. We have locations worldwide, so depending on where you
+                        are, your orders are created and shipped from the facility that can do it most efficiently.</span></p>
+                <h2><strong><span class="fontstyle0">I received a wrong/damaged product, what should I do?</span> </strong></h2>
+                <p><span class="fontstyle0"><br></span><span class="fontstyle2">We’re so sorry if the product you ordered
+                        arrived
+                        damaged. To help us resolve this for you quickly, please email us at
+                        hello@golfcourseprint.com&nbsp;within
+                        a&nbsp;week's time with photos of the damaged product, your order number, and any other details you may
+                        have
+                        about your order. We’ll get back to you with a resolution as soon as possible.</span></p>
+                <h2><strong><span class="fontstyle0">What’s your return policy?</span></strong></h2>
+                <p><span class="fontstyle2">We don’t offer returns and exchanges, but if there’s something wrong with your
+                        order,
+                        please let us know by contacting us at&nbsp;hello@golfcourseprint.com<br></span><span
+                        class="fontstyle0"></span></p>
+                <h2><span class="fontstyle0"><strong>Do you offer refunds?</strong></span></h2>
+                <p><span class="fontstyle2">Refunds are only offered to customers&nbsp;who receive the wrong items or damaged
+                        items.
+                        If any of these apply, please contact us at <span>hello@golfcourseprint.com</span>&nbsp;with photos of
+                        wrong/damaged items and we’ll sort that out for you.<br></span><span class="fontstyle0"></span></p>
+                <h2>
+                    <span class="fontstyle0"><strong>Can I exchange an item for a different size/color?</strong><br></span><span
+                        class="fontstyle2"></span>
+                </h2>
+                <p><span class="fontstyle2">At this time, we don't offer exchanges. If you’re unsure which size would fit
+                        better,
+                        check out our sizing charts—we have one for every item listed on our store, in the product description
+                        section. Though rare, it's possible that an item you ordered was mislabeled. If that’s the case, please
+                        let
+                        us know at <span>hello@golfcourseprint.com</span>&nbsp;within a week&nbsp;of receiving your order.
+                        Include
+                        your order number and photos of the mislabeled item, and we will make it right.&nbsp;</span></p>
             </div>
-
-            <div class="faq">
-                <h2>Q: Will I be charged customs for my order?</h2>
-                <p>An additional customs and tax fee can occur on international orders. This fee is not in our control and is
-                    assessed by your local customs office. Customs policies vary widely for every country so please check with
-                    your local customs office directly to see if they apply duties and taxes to your purchases.</p>
-            </div>
-
-            <div class="faq">
-                <h2>Q: My order should be here by now, but I still don't have it. What should I do?</h2>
-                <p>Before getting in touch with us, please help us out by doing the following:</p>
-                <ul>
-                    <li>Check your shipping confirmation email for any mistakes in the delivery address.</li>
-                    <li>Ask your local post office if they have your package.</li>
-                    <li>Stop by your neighbors in case the courier left the package with them.</li>
-                    <li>If the shipping address was correct, and the package wasn’t left at the post office or at your
-                        neighbor’s, get in touch with us with your order number by emailing <a
-                        href="mailto:hello@coursersprint.com">hello@coursersprint.com</a>.</li>
-                </ul>
-            </div>
-
-            <div class="faq">
-                <h2>Q: How are your products made?</h2>
-                <p>Every order is made to order. We have locations worldwide, so depending on where you are, your orders are
-                    created and shipped from the facility that can do it most efficiently.</p>
-            </div>
-
-            <div class="faq">
-                <h2>Q: I received a wrong/damaged product, what should I do?</h2>
-                <p>We’re so sorry if the product you ordered arrived damaged. To help us resolve this for you quickly, please
-                    email us at <a href="mailto:hello@coursersprint.com">hello@coursersprint.com</a> with a week's time with
-                    photos of the damaged product, your order number, and any other details you may have about your order. We’ll
-                    get back to you with a resolution as soon as possible.</p>
-            </div>
-
-            <div class="faq">
-                <h2>Q: What’s your return policy?</h2>
-                <p>We don’t offer refunds and exchanges, but if there’s something wrong with your order, please let us know by
-                    contacting us at <a href="mailto:hello@coursersprint.com">hello@coursersprint.com</a>.</p>
-            </div>
-
-            <div class="faq">
-                <h2>Q: Do you offer refunds?</h2>
-                <p>Refunds are only offered to customers who receive the wrong items or damaged items. If any of these apply,
-                    please contact us at <a href="mailto:hello@coursersprint.com">hello@coursersprint.com</a> with photos of the
-                    wrong/damaged items and we’ll sort that out for you.</p>
-            </div>
-
-            <div class="faq">
-                <h2>Q: Can I exchange an item for a different size/color?</h2>
-                <p>At this time, we don’t offer exchanges. If you’re unsure which size would fit better, check out our sizing
-                    charts—we have one for every item listed on our site. In the product description section. Though, it’s
-                    possible that in the item ordered was incorrect. If that’s the case, please let us know at <a
-                    href="mailto:hello@coursersprint.com">hello@coursersprint.com</a> with a note of receiving your order
-                    number and photos of the mislabeled item, and we will make it right.</p>
+            <div aria-expanded="true"
+                class="collapse-body collapse in"
+                id="fulfillment-times-tab">
+                <div id="">
+                    <div data-vuex-stores="[]"
+                        data-vue-components="[&quot;dynamic-table&quot;]"
+                        data-vue-root="i1EJuvTb9cSdgTgI">
+                        <div class="container pf-bg-white pf-px-24 pf-pb-32 pf-pt-0">
+                            <div class="row">
+                                <div class="col-12 pf-m-auto">
+                                    <div class="pf-d-md-flex pf-mt-16">
+                                        <div class="pf-d-flex pf-mr-16"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     `;
--- clients/Moreconversion/golfcourse/GC004/src/lib/experiment.js
+++ clients/Moreconversion/golfcourse/GC004/src/lib/experiment.js
@@ -5,11 +5,14 @@ import genericQuestionsWrapper from './components/genericQuestionsWrapper';
 const { ID, VARIATION } = shared;
 
 const init = () => {
+  const productTitleElement = document.querySelector('.h2.product-single__title');
+  const title = productTitleElement.textContent?.split('-')[0] || productTitleElement.textContent;
+
   const targetElement = document.querySelector('.product-full-width');
   if (!document.querySelector(`.${ID}__container`)) {
     targetElement
       .querySelector('.page-width')
-      ?.insertAdjacentHTML('afterbegin', genericQuestionsWrapper(ID));
+      ?.insertAdjacentHTML('afterbegin', genericQuestionsWrapper(ID, title));
   }
 };
 
