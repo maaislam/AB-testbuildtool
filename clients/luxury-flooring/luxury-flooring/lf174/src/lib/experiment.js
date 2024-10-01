ef67fdb51f2d0549b0a199d4d7df9ae352a62f12
SakibAlIslam <sakibalislam12345@gmail.com>
2024-10-01 17:59:25 +0600

    Trade discounts, redesign - done

--- clients/luxury-flooring/luxury-flooring/lf174/src/lib/experiment.js
+++ clients/luxury-flooring/luxury-flooring/lf174/src/lib/experiment.js
@@ -1,3 +1,5 @@
+import usp from './components/usp';
+import { uspData } from './data/data';
 import setup from './services/setup';
 import shared from './shared/shared';
 
@@ -8,9 +10,10 @@ const init = () => {
   const attachPoint = document.querySelector('#maincontent');
 
   if (pathname.includes('/trade-discounts')) {
+    document.body.classList.add(`${ID}__tradeDiscountsPage`);
     const subBanner = () => `<div class="${ID}__subBanner">
       <span class="${ID}__semiBold">Price Match Promise</span>
-      <span> - Found it cheaper? We'll match it!</span>
+      <span>&nbsp;- Found it cheaper? We'll match it!</span>
     </div>`;
 
     const lfClubHTML = () => `<div class="${ID}__lfClub">
@@ -18,55 +21,65 @@ const init = () => {
       </div>
     </div>`;
 
+    const usps = () => `<div class="${ID}__usps">${uspData.map((item) => usp(ID, item)).join('')}</div>`;
+
     const authContainerHTML = () => `<div class="${ID}__authContainer">
       <div class="${ID}__registrationBox">
         <p>Access offers</p>
-        <button class="register-btn">Register now</button>
+        <button class="register-btn" id="${ID}__registerBtn">Register now</button>
       </div>
       <div class="divider"></div>
+      <div class="divider-horizontal"></div>
       <div class="${ID}__loginBox">
         <p>Already have an account?</p>
-        <button class="login-btn">Log in</button>
+        <button class="login-btn" id="${ID}__loginBtn">Log in</button>
       </div>
     </div>`;
 
-    const faqHTML = () => `<div class="${ID}__faqContainer">
-      <div class="${ID}__faqTitle">FAQs</div>
-      <div class="${ID}__faqAccordionWrapper">
-          <div class="${ID}__faqAccordion">
-              <div class="${ID}__faqAccordion-header">
-                  <div class="${ID}__faqAccordion-title">What is the Luxury Flooring Club?</div>
-                  <div class="${ID}__faqAccordion-icon">
-                      <i class="arrow down"></i>
-                  </div>
-              </div>
-              <div class="${ID}__faqAccordion-content">
-                  <p>The Luxury Flooring Club is an exclusive club for our customers. It offers a range of benefits,
-                      including
-                      discounted prices on our products, access to exclusive offers, and more. Sign up today to start
-                      enjoying the benefits of the Luxury Flooring Club.
-                  </p>
-              </div>
-          </div>
-          <div class="${ID}__faqAccordion"></div>
-          <div class="${ID}__faqAccordion-header">
-              <div class="${ID}__faqAccordion-title">What is the Luxury Flooring Club?</div>
-              <div class="${ID}__faqAccordion-icon">
-                  <i class="arrow down"></i>
-              </div>
-          </div>
-          <div class="${ID}__faqAccordion-content">
-              <p>The Luxury Flooring Club is an exclusive club for our customers. It offers a range of benefits, including
-                  discounted prices on our products, access to exclusive offers, and more. Sign up today to start
-                  enjoying the benefits of the Luxury Flooring Club.
-              </p>
-          </div>
-      </div>
+    const faqHTML = () => `
+    <div class="${ID}__faqContainer">
+        <div class="${ID}__faqWrapper">
+            <div class="${ID}__faqTitle">FAQs</div>
+
+            <div class="${ID}__faqAccordionWrapper">
+                <div class="${ID}__faqAccordion">
+                    <div class="${ID}__faqAccordion-header">
+                        <div class="${ID}__faqAccordion-title">What is the Luxury Flooring Club?</div>
+                        <div class="${ID}__faqAccordion-icon">
+                            <i class="arrow down"></i>
+                        </div>
+                    </div>
+                    <div class="${ID}__faqAccordion-content">
+                        <p>The Luxury Flooring Club is an exclusive club for our customers. It offers a range of benefits,
+                            including
+                            discounted prices on our products, access to exclusive offers, and more. Sign up today to start
+                            enjoying the benefits of the Luxury Flooring Club.
+                        </p>
+                    </div>
+                </div>
+                <div class="${ID}__faqAccordion">
+                    <div class="${ID}__faqAccordion-header">
+                        <div class="${ID}__faqAccordion-title">What is the Luxury Flooring Club?</div>
+                        <div class="${ID}__faqAccordion-icon">
+                            <i class="arrow down"></i>
+                        </div>
+                    </div>
+                    <div class="${ID}__faqAccordion-content">
+                        <p>The Luxury Flooring Club is an exclusive club for our customers. It offers a range of benefits,
+                            including
+                            discounted prices on our products, access to exclusive offers, and more. Sign up today to start
+                            enjoying the benefits of the Luxury Flooring Club.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>`;
 
     const mainContainer = `<div class="${ID}__mainContainer">
       ${subBanner()}
       ${lfClubHTML()}
+      ${usps()}
       ${authContainerHTML()}
       ${faqHTML()}
     </div>`;
@@ -100,6 +113,10 @@ export default () => {
 
       //Toggle arrow direction
       icon.classList.toggle('down');
+    } else if (target.id === `${ID}__registerBtn`) {
+      window.location.href = '/trade-registration';
+    } else if (target.id === `${ID}__loginBtn`) {
+      window.location.href = '/customer/account/';
     }
   });
 };
