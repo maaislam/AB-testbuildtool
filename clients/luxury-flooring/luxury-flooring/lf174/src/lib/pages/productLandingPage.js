bb3630ee79a82e55d4d132b3908d52ec7fd25e90
SakibAlIslam <sakibalislam12345@gmail.com>
2024-10-04 19:41:44 +0600

    plp text added

--- clients/luxury-flooring/luxury-flooring/lf174/src/lib/pages/productLandingPage.js
+++ clients/luxury-flooring/luxury-flooring/lf174/src/lib/pages/productLandingPage.js
@@ -1,17 +1,33 @@
+/*eslint-disable no-param-reassign */
 import banner from '../components/banner';
 import subBanner from '../components/subBanner';
-import { pollerLite } from '../helpers/utils';
+import { observeDOM, pollerLite } from '../helpers/utils';
+
+const setTradePriceText = () => {
+    const onlyPriceElem = document.querySelectorAll('.only-price');
+    onlyPriceElem.forEach((item) => {
+        if (!item.textContent.includes('Trade Price')) {
+            item.textContent = 'Trade Price:';
+        }
+    });
+};
 
 const productLandingPage = (ID) => {
     document.body.classList.add(`${ID}__plp`);
 
-    pollerLite(['.nav-sections'], () => {
+    pollerLite(['.nav-sections', '.only-price'], () => {
         const attachPoint = document.querySelector('.nav-sections');
 
         if (!document.querySelector(`.${ID}__banner`)) {
             const banners = `${banner(ID)}${subBanner(ID)}`;
             attachPoint.insertAdjacentHTML('afterend', banners);
         }
+
+        setTradePriceText();
+
+        observeDOM('.products.products-grid', () => {
+            setTradePriceText();
+        });
     });
 };
 export default productLandingPage;
