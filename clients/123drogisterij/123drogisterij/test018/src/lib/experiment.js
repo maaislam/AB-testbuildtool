9f6dae7fa29c3c03328da7262ffc491a5aac7862
raihankabir36850 <raihanku320@gmail.com>
2024-10-08 18:37:22 +0600

    new test 123drog

---
+++ clients/123drogisterij/123drogisterij/test018/src/lib/experiment.js
@@ -0,0 +1,120 @@
+import setup from './services/setup';
+import gaTracking from './services/gaTracking';
+import shared from './shared/shared';
+import { pollerLite } from './helpers/utils';
+import initialProductsFetch from './helpers/initialProductsFetch';
+import notification from './components/notification';
+
+const { ID, VARIATION } = shared;
+
+const formatPrice = (amount, code = 'en-GB', currency = 'EUR') =>
+  new Intl.NumberFormat(code, {
+    style: 'currency',
+    currency,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }).format(amount);
+
+const init = () => {
+  const cartItems = document.querySelectorAll('.cart.item .item-info');
+
+  const fetchPromises = Array.from(cartItems).map((cartItem) => {
+    cartItem.classList.add(`${ID}__cartItem`);
+    const url = cartItem.querySelector('a').getAttribute('href');
+    return initialProductsFetch(url);
+  });
+
+  Promise.all(fetchPromises)
+    .then((docs) => {
+      const totalSavings = docs.reduce((accumulatedSavings, doc, index) => {
+        const upsellOptions = doc.querySelectorAll('.custom-child-upsel-checkbox input');
+        const finalPriceElement = doc.querySelector('span[data-price-type="finalPrice"]');
+        const mainPrice = finalPriceElement.dataset.priceAmount;
+        const rowItem = document.querySelectorAll(`.${ID}__cartItem`)[index];
+        const mainTable = rowItem.closest('#shopping-cart-table');
+        const priceTag = mainTable.querySelector('thead th.col.price:not(.main-price-tag)');
+        const priceValueElement = rowItem.querySelector('td.col.price');
+        const qtyInputElem = rowItem.querySelector('.input-text.qty');
+        const subTotal = rowItem
+          .querySelector('td.col.subtotal span.price')
+          .innerText.replace(/[^\d,.-]/g, '')
+          .replace(',', '.')
+          .trim();
+
+        const qtyValue = qtyInputElem.value;
+
+        upsellOptions.forEach((upsellOption, upsellIndex) => {
+          const inputValue = upsellOption.value;
+          if (inputValue > 1) {
+            rowItem.setAttribute(`data-bulk-option-${upsellIndex + 1}`, inputValue);
+            parseInt(qtyValue) >= parseInt(inputValue) &&
+              rowItem.setAttribute('data-multipack', true);
+          }
+        });
+
+        let itemSavings = 0;
+
+        if (rowItem.getAttribute('data-multipack')) {
+          itemSavings = parseFloat(mainPrice) * qtyValue - parseFloat(subTotal);
+          priceTag.querySelector('span').innerText = 'Actie Prijs';
+          if (!mainTable.querySelector('.main-price-tag')) {
+            priceTag.insertAdjacentHTML(
+              'beforebegin',
+              '<th class="col price main-price-tag" scope="col"><span>Prijs</span></th>'
+            );
+          }
+
+          priceValueElement.setAttribute('data-th', 'Actie Prijs');
+
+          if (!rowItem.querySelector('.main-price')) {
+            priceValueElement.insertAdjacentHTML(
+              'beforebegin',
+              `<td class="col price main-price has-multipack" data-th="Prijs" >
+              <span class="price-including-tax" data-label="Incl. BTW">
+                <span class="cart-price">
+                  <span class="price">${formatPrice(mainPrice).replace('.', ',')}</span>
+                </span>
+              </span>
+            </td>`
+            );
+          }
+        } else {
+          const price = priceValueElement.querySelector('span.price').innerText;
+          if (!rowItem.querySelector('.main-price')) {
+            priceValueElement.insertAdjacentHTML(
+              'beforebegin',
+              `<td class="col price main-price" data-th="Prijs">
+              <span class="price-including-tax" data-label="Incl. BTW">
+                <span class="cart-price">
+                  <span class="price">${price}</span>
+                </span>
+              </span>
+            </td>`
+            );
+          }
+        }
+
+        //Add the item's savings to the accumulated total
+        return accumulatedSavings + itemSavings + 4.95;
+      }, 0);
+
+      //Log the total savings for all cart items
+      if (formatPrice(totalSavings) && !document.querySelector(`.${ID}__upsell-notification`)) {
+        const cartSummary = document.querySelector('.cart-summary');
+        cartSummary.insertAdjacentHTML(
+          'beforeend',
+          notification(ID, formatPrice(totalSavings), 'active')
+        );
+      }
+    })
+    .catch((error) => {
+      console.log('An error occurred during Promise.all:', error);
+    });
+};
+
+export default () => {
+  setup();
+  console.log(ID);
+
+  init();
+};
---
+++ clients/Moreconversion/Arrowhead/AH020/src/lib/data/data.js
@@ -0,0 +1,148 @@
+const collectionArray = [
+  {
+    name: 'rose-carrier-joggers',
+    link: '/collections/womens-carry-joggers'
+  },
+  {
+    name: 'carrier-sweatpants',
+    link: '/collections/carrier-sweatpants'
+  },
+  {
+    name: 'carrier-joggers-mk-ii',
+    link: '/collections/carrier-joggers-mk-ii'
+  },
+  {
+    name: 'carrier-traveler-joggers',
+    link: '/collections/carrier-traveler-joggers'
+  },
+  {
+    name: 'concealment-hoodie',
+    link: '/collections/concealment-hoodies'
+  },
+  {
+    name: 'durable-hoodie',
+    link: '/collections/concealment-hoodies'
+  },
+  {
+    name: 'durable-quarter-zip',
+    link: '/collections/concealment-quarter-zips'
+  },
+  {
+    name: 'tactical-jacket',
+    link: '/collections/concealment-jackets'
+  },
+  {
+    name: 'concealtee™',
+    link: '/collections/carrier-concealtee™'
+  },
+  {
+    name: 'carrier-training-shorts',
+    link: '/collections/carrier-training-shorts'
+  },
+  {
+    name: 'carrier-shorts-mk-ii',
+    link: '/collections/carrier-shorts-mk-ii'
+  },
+  {
+    name: 'carrier-shorts-mk-ii-midnight-black-7',
+    link: '/collections/carrier-shorts-7-inseam'
+  },
+  {
+    name: 'carrier-shorts-mk-ii-olive-drab-7',
+    link: '/collections/carrier-shorts-7-inseam'
+  },
+  {
+    name: 'carrier-shorts-mk-ii-slate-grey-7',
+    link: '/collections/carrier-shorts-7-inseam'
+  },
+  {
+    name: 'new-carrier-shorts-mk-ii-sandman-7',
+    link: '/collections/carrier-shorts-7-inseam'
+  },
+  {
+    name: 'carrier-shorts-8-midnight-black',
+    link: '/collections/carrier-shorts-8-inseam'
+  },
+  {
+    name: 'carrier-shorts-8-olive-drab',
+    link: '/collections/carrier-shorts-8-inseam'
+  },
+  {
+    name: 'new-carrier-shorts-8-navy-blue',
+    link: '/collections/carrier-shorts-8-inseam'
+  },
+  {
+    name: 'carrier-shorts-8-chalk-grey',
+    link: '/collections/carrier-shorts-8-inseam'
+  },
+  {
+    name: 'carrier-shorts-8-founders-edition-1',
+    link: '/collections/carrier-shorts-8-inseam'
+  },
+  {
+    name: 'carrier-shorts-11',
+    link: '/collections/carrier-shorts-11-inseam'
+  },
+  {
+    name: 'carrier-shorts-mk-ii-midnight-black-11',
+    link: '/collections/carrier-shorts-11-inseam'
+  },
+  {
+    name: 'carrier-shorts-mk-ii-olive-drab-11',
+    link: '/collections/carrier-shorts-11-inseam'
+  },
+  {
+    name: 'new-carrier-shorts-mk-ii-slate-grey-11',
+    link: '/collections/carrier-shorts-11-inseam'
+  },
+  {
+    name: 'rose-carrier-shorts-black',
+    link: '/collections/rose-carrier-shorts'
+  },
+  {
+    name: 'holster',
+    link: '/collections/holsters-and-mag-carriers'
+  },
+  {
+    name: 'appendix',
+    link: '/collections/holsters-and-mag-carriers'
+  },
+  {
+    name: 'the-5-oclocker',
+    link: '/collections/holsters-and-mag-carriers'
+  },
+  {
+    name: 'the-paddler-hip-duty',
+    link: '/collections/holsters-and-mag-carriers'
+  },
+  {
+    name: 'rapid-clasp-retention-belt',
+    link: '/collections/holsters-and-mag-carriers'
+  },
+  {
+    name: 'omnicarry-retention-belt',
+    link: '/collections/holsters-and-mag-carriers'
+  },
+  {
+    name: 'performance-hat',
+    link: '/collections/holsters-and-mag-carriers'
+  },
+  {
+    name: 'limited-edition-upside-down-hat',
+    link: '/collections/holsters-and-mag-carriers'
+  },
+  {
+    name: 'covert-iwb-magazine-carrier',
+    link: '/collections/holsters-and-mag-carriers'
+  },
+  {
+    name: 'carrier-rapid-clasp-retention-belt',
+    link: '/collections/holsters-and-mag-carriers'
+  },
+  {
+    name: 'enhanced-rapid-clasp-retention-belt',
+    link: '/collections/holsters-and-mag-carriers'
+  }
+];
+
+export default collectionArray;
