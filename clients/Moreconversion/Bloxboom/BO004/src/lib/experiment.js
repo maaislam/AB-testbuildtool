834fa4b0b3d8ee28d88776f3cf574d911bf47d67
SakibAlIslam <sakibalislam12345@gmail.com>
2024-10-28 18:33:57 +0600

    BO004 - add badge for V1 & V2

--- clients/Moreconversion/Bloxboom/BO004/src/lib/experiment.js
+++ clients/Moreconversion/Bloxboom/BO004/src/lib/experiment.js
@@ -1,7 +1,8 @@
+import badge from './components/badge';
 import setup from './services/setup';
 import shared from './shared/shared';
 
-const { ID } = shared;
+const { ID, VARIATION } = shared;
 
 const extractNumber = (str) => {
   const numberMatch = str.match(/[\d.]+/);
@@ -11,11 +12,22 @@ const extractNumber = (str) => {
 
 const init = () => {
   const modalButtons = document.querySelectorAll('section[role="dialog"] div button[data-price]');
+  let largestBundle = 0;
+  let largestBundleButton = null;
+
   modalButtons.forEach((button) => {
     const price = button.getAttribute('data-price');
     const priceNumber = extractNumber(price);
-    console.log('priceNumber: ', priceNumber);
+
+    if (priceNumber > largestBundle) {
+      largestBundle = priceNumber;
+      largestBundleButton = button;
+    }
   });
+
+  if (largestBundleButton) {
+    largestBundleButton.insertAdjacentHTML('afterbegin', badge(ID, VARIATION));
+  }
 };
 
 export default () => {
