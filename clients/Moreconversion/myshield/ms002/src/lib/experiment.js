41c72189305c403f6be92877988b5cbd29107f07
SakibAlIslam <sakibalislam12345@gmail.com>
2024-11-14 03:20:21 +0600

    ms002 done

--- clients/Moreconversion/myshield/ms002/src/lib/experiment.js
+++ clients/Moreconversion/myshield/ms002/src/lib/experiment.js
@@ -1,18 +1,29 @@
 import setup from './services/setup';
-//import shared from './shared/shared';
+import shared from './shared/shared';
 
-//const { ID, VARIATION } = shared;
+const { ID } = shared;
 
 const init = () => {
   const beforeAfterSlider = document.querySelector('.multicolumn .slider-mobile-gutter');
   const beforeAfterSection = beforeAfterSlider.closest('.shopify-section');
   const beforeAfterHeading = beforeAfterSection.previousSibling;
+  const beforeAfterCta = beforeAfterSection.nextElementSibling;
 
-  const benifitsDesktop = document.querySelector('.benefits-homepage-desktop');
+  const benifitsDesktop = document.querySelector('[src*="clear_skin_guaranteed_benefits"]');
   const benifitsSection = benifitsDesktop.closest('.shopify-section');
 
-  benifitsSection.insertAdjacentElement('beforebegin', beforeAfterHeading);
-  benifitsSection.insertAdjacentElement('beforebegin', beforeAfterSection);
+  const livesChangeCountHtml = `<div class="${ID}__livesChangeCountWrapper">
+    <div class="${ID}__livesChangeCount">10,017 lives changed!</div>
+  </div>`;
+
+  beforeAfterSection.classList.add(`${ID}__beforeAfterSection`);
+  beforeAfterCta.classList.add(`${ID}__beforeAfterCta`);
+  if (!document.querySelector(`.${ID}__livesChangeCountWrapper`)) {
+    benifitsSection.insertAdjacentElement('beforebegin', beforeAfterHeading);
+    benifitsSection.insertAdjacentHTML('beforebegin', livesChangeCountHtml);
+    benifitsSection.insertAdjacentElement('beforebegin', beforeAfterSection);
+    benifitsSection.insertAdjacentElement('beforebegin', beforeAfterCta);
+  }
 };
 
 export default () => {
