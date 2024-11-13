6707bc1d9fbaa9cc780e6987793e9bdd3b5279c1
SakibAlIslam <sakibalislam12345@gmail.com>
2024-11-14 02:52:47 +0600

    ms001 done

--- clients/Moreconversion/myshield/ms001/src/lib/experiment.js
+++ clients/Moreconversion/myshield/ms001/src/lib/experiment.js
@@ -2,9 +2,11 @@ import startTimer from './helpers/startTimer';
 import setup from './services/setup';
 import shared from './shared/shared';
 
-const { ID } = shared;
+const { ID, VARIATION } = shared;
 
 const endDate = '12/03/2024'; //MM/DD/YYYY
+
+const isMobile = () => window.matchMedia('(max-width: 768px)').matches;
 
 const init = () => {
   const attachPoint = document.querySelector('.announcement-bar-section');
@@ -36,16 +38,46 @@ const init = () => {
         </div>
       </div>
       <div class='${ID}__banner-right'>
-        <a href="" class='${ID}__banner-right-btn ${ID}__banner-right-btn--desktop'>View Offer Details →</a>
-        <a href="" class='${ID}__banner-right-btn ${ID}__banner-right-btn--mobile'>View Offer →</a>
+        <a class='${ID}__banner-right-btn ${ID}__banner-right-btn--desktop' onclick="document.querySelector('#offersection').scrollIntoView({ behavior: 'smooth' }); return false;">View Offer Details →</a>
+        <a class='${ID}__banner-right-btn ${ID}__banner-right-btn--mobile' onclick="document.querySelector('#offersection').scrollIntoView({ behavior: 'smooth' }); return false;">View Offer →</a>
       </div>
     </div>
   </div>
   `;
 
-  if (!document.querySelector(`.${ID}__banner`)) {
+  if (!document.querySelector(`.${ID}__banner`) && VARIATION === '1') {
     attachPoint.insertAdjacentHTML('beforebegin', htmlStr);
-    startTimer(ID, endDate);
+    startTimer(ID);
+  }
+
+  if (!document.querySelector(`.${ID}__heroBannerWrapper`) && VARIATION === '2') {
+    const flashSaleBlock = document.querySelector('.flash-sale-block');
+    const cloneFlashSaleBlock = flashSaleBlock.cloneNode(true);
+    cloneFlashSaleBlock.classList.add(`${ID}__heroBannerWrapper`);
+    const leftSideHero = isMobile() ? document.querySelector('.text-container2') : document.querySelector('.left-side-hero');
+
+    leftSideHero.appendChild(cloneFlashSaleBlock);
+
+    const heroBanner = document.querySelector(`.${ID}__heroBannerWrapper`);
+    const title = heroBanner.querySelector('h1');
+    const getOffText = heroBanner.querySelector('.CTA-text-countdown p');
+    const moneybackText = heroBanner.querySelector('.CTA-text-countdown p:nth-child(2)');
+    const countdown = heroBanner.querySelector('.countdown');
+    const countDownHtml = `<div class="countdown-timer apply-grid show-day">
+      <span class="value hou" id="${ID}__hours"></span>
+      <span class="value min" id="${ID}__minutes"></span>
+      <span class="value sec" id="${ID}__seconds"></span>
+      <span class="label hou">Hours</span>
+      <span class="label min">Minutes</span>
+      <span class="label sec">Seconds</span>
+    </div>`;
+
+    title.innerHTML = 'DON’T MISS OUT OF SHIELD’S <br>EARLY BLACK FRIDAY SALE!';
+    getOffText.innerHTML = '50% off your first purchase!';
+    moneybackText.innerHTML = 'Plus our 100 day trial period <br>moneyback guarantee';
+    countdown.innerHTML = '';
+    countdown.insertAdjacentHTML('afterend', countDownHtml);
+    startTimer(ID);
   }
 };
 
