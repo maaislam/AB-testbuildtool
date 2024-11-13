e2a6de2bcc19edaec50894bfb5a3d55a9f25e78a
raihankabir36850 <raihanku320@gmail.com>
2024-11-14 02:23:34 +0600

    code refactor

--- clients/browsertobuyer/sikkerrejse/dl12/src/lib/global.js
+++ clients/browsertobuyer/sikkerrejse/dl12/src/lib/global.js
@@ -1,7 +1,6 @@
 import { pollerLite } from './helpers/utils';
 
 export default () => {
-  console.log('hello');
   document.body.addEventListener('click', (e) => {
     const { target } = e;
     if (target.closest('button.open-input')) {
