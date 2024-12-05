529386cbfb1b6c0dc1163128cf7b845ff8ee3cea
SakibAlIslam <sakibalislam12345@gmail.com>
2024-12-05 16:30:01 +0600

    Update image and video sources in experiment module; adjust pathname check for socks

--- clients/Moreconversion/perfectwhitetee/pwt016/src/index.js
+++ clients/Moreconversion/perfectwhitetee/pwt016/src/index.js
@@ -1,6 +1,6 @@
 import activate from './lib/experiment';
 import { pollerLite } from './lib/helpers/utils';
 
-if (window.location.pathname.includes('socks')) {
+if (window.location.pathname.includes('sock')) {
     pollerLite(['.product-main-slide[data-index="0"] .image-wrap'], activate);
 }
--- clients/Moreconversion/perfectwhitetee/pwt016/src/lib/experiment.js
+++ clients/Moreconversion/perfectwhitetee/pwt016/src/lib/experiment.js
@@ -8,8 +8,8 @@ const init = () => {
   const firstThumb = document.querySelector('.product__thumb[data-index="0"] img');
 
   if (firstThumb) {
-    firstThumb.src = 'https://perfectwhitetee.com/cdn/shop/files/YAP_sock2_100x.jpg?v=1731510939';
-    firstThumb.srcset = 'https://perfectwhitetee.com/cdn/shop/files/YAP_sock2_100x.jpg?v=1731510939';
+    firstThumb.src = 'https://cdn.shopify.com/s/files/1/0414/8677/8528/files/LOREN_LEAH_WHITE_2275.jpg?v=1732201637';
+    firstThumb.srcset = 'https://cdn.shopify.com/s/files/1/0414/8677/8528/files/LOREN_LEAH_WHITE_2275.jpg?v=1732201637';
   }
 
   if (firstSlide) {
@@ -23,7 +23,6 @@ const init = () => {
     video.style.width = '100%';
     video.style.height = 'auto';
 
-    //Programmatically set autoplay and muted attributes
     video.autoplay = true; //This ensures autoplay
     video.muted = true; //Ensure video is muted for autoplay to work
     video.controls = false; //Remove controls if not needed
@@ -32,7 +31,7 @@ const init = () => {
     video.preload = 'auto'; //This ensures the video will preload
 
     const source = document.createElement('source');
-    source.src = 'https://videos.pexels.com/video-files/3205919/3205919-hd_1080_1920_25fps.mp4';
+    source.src = 'https://cdn.shopify.com/videos/c/o/v/424000f1fc164d7a94dc8f1514bf6928.mp4';
     source.type = 'video/mp4';
 
     video.appendChild(source);
