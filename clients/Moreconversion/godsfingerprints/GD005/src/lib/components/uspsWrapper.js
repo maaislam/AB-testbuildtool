0249d12f87ec49d12afa33f51d26bdbbb1908770
SakibAlIslam <sakibalislam12345@gmail.com>
2024-12-13 19:17:26 +0600

    Refactor experiment styles and update USPS wrapper content for clarity and responsiveness

--- clients/Moreconversion/godsfingerprints/GD005/src/lib/components/uspsWrapper.js
+++ clients/Moreconversion/godsfingerprints/GD005/src/lib/components/uspsWrapper.js
@@ -7,22 +7,22 @@ const uspsWrapper = (id) => {
             <div class="${id}__usps-item">
                 <div class="${id}__icon">${guaranteeIcon}</div>
                 <div class="${id}__textWrapper">
-                  <p class="${id}__highlight">100%</p>
-                  <p class="${id}__text">SATISFACTION GUARANTEE</p>
+                  <p class="${id}__highlight">100% Satisfaction Guarantee</p>
+                  <p class="${id}__text">Free replacements and refunds</p>
                 </div>
             </div>
             <div class="${id}__usps-item">
                 <div class="${id}__icon">${freeplacementIcon}</div>
                 <div class="${id}__textWrapper">
-                  <p class="${id}__highlight">FREE</p>
-                  <p class="${id}__text">REPLACEMENTS &&nbsp;REFUNDS</p>
+                  <p class="${id}__highlight">Premium museum quality art prints</p>
+                  <p class="${id}__text">All prints and frames are made in the USA</p>
                 </div>
             </div>
             <div class="${id}__usps-item">
                 <div class="${id}__icon">${preminumIcon}</div>
                 <div class="${id}__textWrapper">
-                  <p class="${id}__highlight">PREMIUM</p>
-                  <p class="${id}__text">MUSEUM QUALITY ART PRINTS</p>
+                  <p class="${id}__highlight">Every purchase supports vulnerable children</p>
+                  <p class="${id}__text">Every order helps fund Alongsiders International</p>
                 </div>
             </div>
       </div>
--- clients/Moreconversion/godsfingerprints/GD005/src/lib/experiment.js
+++ clients/Moreconversion/godsfingerprints/GD005/src/lib/experiment.js
@@ -2,13 +2,11 @@ import setup from './services/setup';
 import shared from './shared/shared';
 import uspsWrapper from './components/uspsWrapper';
 
-const { ID, VARIATION } = shared;
+const { ID } = shared;
 
 const init = () => {
-  //Your experiment code here
-
   const targetElement = document.querySelector('.product [data-product-block="quantity_selector"]');
-  console.log(targetElement, 'targetElement');
+
   if (!document.querySelector(`.${ID}__usps-wrapper`)) {
     targetElement.insertAdjacentHTML('beforebegin', uspsWrapper(ID));
   }
