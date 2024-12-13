53ffe17e3056fa599cc01b619a83a0b0aadfe9e4
SakibAlIslam <sakibalislam12345@gmail.com>
2024-12-13 18:45:43 +0600

    Update pollerLite selector and add responsive image styling in experiment.scss

--- clients/browsertobuyer/davidaustin/DAR078/src/lib/data/data.js
+++ clients/browsertobuyer/davidaustin/DAR078/src/lib/data/data.js
@@ -6,7 +6,7 @@ export const plpData = {
                 title: 'Climbing Roses',
                 description: 'Climbing Roses are an excellent way of bringing height and a feeling of abundance to the garden.',
                 url: '/collections/climbing-roses',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Climbing_Roses_675c23a2bcabb.png'
             },
             {
                 title: 'Standard Tree Roses',
@@ -30,13 +30,13 @@ export const plpData = {
                 title: 'English Roses',
                 description: 'David Austin has spent the last sixty years developing his award-winning English Roses. They combine the delicate charm and wonderful fragrances of the Old Roses with the wider colour range and repeat-flowering nature of modern roses.',
                 url: '/collections/english-roses',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/English_Roses_675c243642448.png'
             },
             {
                 title: 'Floribundas',
                 description: 'Floribunda roses were originally produced by crossing Hybrid Teas with Polyantha Roses. They are prized for creating a mass of colour by bearing many flowers held in large clusters.',
                 url: '/collections/floribundas',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Floribunda_675c254bb31db.png'
             },
             {
                 title: 'Ground Cover Roses',
@@ -48,7 +48,7 @@ export const plpData = {
                 title: 'Hybrid Teas',
                 description: 'The beauty of Hybrid Tea Roses lies in their exquisitely formed, pointed ‘bud flowers’. Each stem usually holds a single flower with sparse foliage at the base.',
                 url: '/collections/hybrid-teas',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Hybrid_Teas_675c25995c6e1.png'
             },
             {
                 title: 'Old Roses',
@@ -60,7 +60,7 @@ export const plpData = {
                 title: 'Wild Roses',
                 description: 'We offer wild roses from many parts of the world, together with their immediate hybrids. These include some very fine shrub roses notable for the elegance of their growth, their dainty foliage and the simple beauty of their flowers.',
                 url: '/collections/wild-and-scottish-roses',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/74fd7950b3de4eaebe3a6bde60ceedac_b34c977c-7ffc-4994-9831-2f72a9b82961.jpg?v=1595540498'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Wild_Roses_675c26eef03aa.png'
             }
         ]
     },
@@ -71,19 +71,19 @@ export const plpData = {
                 title: 'Apricot & Orange',
                 description: 'Apricot and orange roses are a wonderful way of bringing warmth to the border.',
                 url: '/collections/apricot-orange-roses',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Apricot_&_Orange_675c234dbd4cd.png'
             },
             {
                 title: 'Pink',
                 description: 'Pink is the master of adaptability. Simply by planting with different combinations of pink roses you can create a myriad of styles.',
                 url: '/collections/pink-roses',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Pink_675c25ed65be8.png'
             },
             {
                 title: 'White & Cream',
                 description: 'There is something quite special about white roses that is hard to explain – they are all purity and light. The majority of varieties we call white are actually creamy white with tints of apricot or pink. However, the overall effect in the mass is often pure white.',
                 url: '/collections/white-cream-roses',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/White_&_Cream_675c26d515a14.png'
             },
             {
                 title: 'Red',
@@ -95,10 +95,10 @@ export const plpData = {
                 title: 'Yellow',
                 description: 'Yellow roses are a welcome addition to any garden – their colouring seeming to denote happiness and sunshine.',
                 url: '/collections/yellow-roses',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Yellow_675c270bcc8d2.png'
             },
             {
-                title: 'Purple Roses',
+                title: 'Purple',
                 description: 'Yellow roses are a welcome addition to any garden – their colouring seeming to denote happiness and sunshine.',
                 url: '/collections/purple-roses',
                 imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/ef0dbadb1f73186e884e3257f58b9c57_c20a5543-64d9-4587-ad09-712b8cf85793.jpg?v=1595547220'
@@ -112,49 +112,49 @@ export const plpData = {
                 title: 'Mixed Borders',
                 description: 'Roses are marvellously versatile, suiting almost every garden situation, and one of the most popular is within a mixed border.',
                 url: '/collections/ideal-for-mixed-border',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Mixed_Borders_675c25be4d784.png'
             },
             {
                 title: 'Pots & Containers',
                 description: 'Roses are wonderful plants for growing in pots, urns and other containers. Roses in pots can be grown in even the smallest amount of outside space, such as on a city balcony or terrace.',
                 url: '/collections/ideal-for-pots-containers',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Pots_&_Containers_675c260666db3.png'
             },
             {
                 title: 'Shady Areas',
                 description: 'Planting roses in shady positions is a great way of bringing colour and interest to forgotten areas of the garden.',
                 url: '/collections/ideal-for-north-walls-and-shady-areas',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Shady_Areas_675c26bd35729.png'
             },
             {
                 title: 'Rose Hedges',
                 description: 'Hedges of David Austin’s English Roses are particularly valuable. Unlike most other hedges they will repeat flower prolifically, producing flushes of glorious, fragrant blooms throughout the summer until the first frosts.',
                 url: '/collections/ideal-for-growing-as-hedges',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Rose_Hedges_675c2621c3817.png'
             },
             {
-                title: '6ft Wall or Fence',
+                title: '6ft wall or fence',
                 description: 'Most gardens have at least one standard height (6ft/1.8m) fence or wall, which is often dull or simply ignored. Reinvigorate this space by clothing it in one of these beautiful, fragrant roses.',
                 url: '/collections/ideal-for-walls-fences',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/6ft_wall_or_fence_675c1f423a94f.png'
             },
             {
-                title: '10ft Wall or Fence',
+                title: '10ft wall or fence',
                 description: 'By training a fabulous, fragrant climbing rose up a wall or fence, you can convert a dull, even unsightly area into one of the most eye-catching features in your garden.',
                 url: '/collections/ideal-for-10ft-wall-and-fence',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/10ft_wall_or_fence_675c232a053f9.png'
             },
             {
                 title: 'Front of a house',
                 description: 'Brighten up the front of your house with a magnificent climbing rose. A climbing rose can transform any home with its colourful blooms.',
                 url: '/collections/ideal-for-buildings',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Front_of_a_house_675c256665560.png'
             },
             {
-                title: 'Arches and Pergolas',
+                title: 'Arches & Pergolas',
                 description: 'Browse our selection of beautiful roses for garden archways.  A rose arch adds height and formality to a garden, linking different areas and formatting an entranceway with flowers overhead.',
                 url: '/collections/ideal-for-arches',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Arches_&_Pergolas_675c236fa9ed8.png'
             }
         ]
     },
@@ -165,43 +165,43 @@ export const plpData = {
                 title: 'Most fragrant roses',
                 description: 'English Roses are famous for the diversity and strength of their fragrances, with many varieties having won awards for their delicious fragrances',
                 url: '/collections/most-fragrant',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Mixed_Borders_675c25be4d784.png'
             },
             {
                 title: 'Roses best for flowering',
                 description: 'Roses flower more abundantly than almost any other garden plant, providing colour and blooms right through the summer months and beyond.',
                 url: '/collections/best-for-abundantly-flowering',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Roses_best_for_flowering_675c267301518.png'
             },
             {
                 title: 'Most disease resistant roses',
                 description: 'The easiest way to grow a rose well is to choose a strong, disease resistant variety and here we have listed the most disease resistant roses in our collection.',
                 url: '/collections/disease-resistant-roses',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Most_disease_resistant_roses_675c25d778d5c.png'
             },
             {
                 title: 'Roses best for shady areas',
                 description: 'Planting roses in shady positions is a great way of bringing colour and interest to forgotten areas of the garden.',
                 url: '/collections/ideal-for-north-walls-and-shady-areas',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Shady_Areas_675c26bd35729.png'
             },
             {
                 title: 'Roses with very few thorns',
                 description: 'Roses with very few thorns are ideal for planting in areas that you frequently walk past or sit near. They are also especially useful in gardens where children and pets play.',
                 url: '/collections/ideal-for-thornless',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Roses_with_very_few_thorns_675c269b32830.png'
             },
             {
                 title: 'Roses best for attracting bees',
                 description: 'Bees and insects are incredibly valuable to have in the garden. Beneficial insects will help to keep your roses healthy by eating pests such as aphids.',
                 url: '/collections/ideal-for-bee-friendly-roses',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Roses_best_for_attracting_bees_675c26451debd.png'
             },
             {
                 title: 'Roses best for cutting',
                 description: 'One of the most enjoyable aspects of growing roses in your garden is being able to cut a few blooms, and transporting their beauty and scent indoors.',
                 url: '/collections/cuttings-for-arrangements',
-                imgSrc: 'https://www.davidaustinroses.co.uk/cdn/shop/products/Rosa-_Claire-Austin__13A9207.20200810113524277_web.jpg?v=1616082747'
+                imgSrc: 'https://cdn-3.convertexperiments.com/uf/10021806/10025468/Roses_best_for_cutting_675c265f078fa.png'
             }
         ]
     }
