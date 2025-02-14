85f5a7672676738f0e112952ded7b0dfcccaa45a
maaislam <maaislam@outlook.com>
2025-02-15 02:27:34 +0600

    checking husky

--- clients/luxury-flooring/luxury-flooring/lf001/src/index.js
+++ clients/luxury-flooring/luxury-flooring/lf001/src/index.js
@@ -1,6 +1,6 @@
 import activate from './lib/experiment';
 import { pollerLite } from './lib/helpers/utils';
 
-if (window.location.href.includes('/checkout') && !window.location.href.includes('/success')) {
+if (window.location.href.includes('/checkou') && !window.location.href.includes('/success')) {
   pollerLite(['.nav-sections-items', '.minicart-wrapper'], activate);
 }
