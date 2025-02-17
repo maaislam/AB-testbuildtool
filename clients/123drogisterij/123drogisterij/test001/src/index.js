e933725b5be987707798e8669fe28f0fbfe115b0
maaislam <maaislam@outlook.com>
2025-02-18 02:09:56 +0600

    checking workflow

--- clients/123drogisterij/123drogisterij/test001/src/index.js
+++ clients/123drogisterij/123drogisterij/test001/src/index.js
@@ -1,8 +1,4 @@
 import activate from './lib/experiment';
 import { pollerLite } from '../../../../../globalUtil/util';
 
-const ieChecks = /MSIE|Trident|Edge\/(12|13|14|15|16|17|18)/.test(window.navigator.userAgent);
-
-if (!ieChecks) {
-  pollerLite(['.product-info-main', '#product-addtocart-button'], activate);
-}
+pollerLite(['.product-info-main', '#product-addtocart-button'], activate);
