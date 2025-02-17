9aedf173ae705876fbc34a85929d6de9758c4e48
maaislam <maaislam@outlook.com>
2025-02-18 02:12:50 +0600

    checking workflow

--- clients/123drogisterij/123drogisterij/test001/src/lib/experiment.js
+++ clients/123drogisterij/123drogisterij/test001/src/lib/experiment.js
@@ -9,7 +9,7 @@ import shared from './shared/shared';
 const { ID, fdifbisdfisfbis } = shared;
 
 export default () => {
-  if (sessionStorage.getItem(reloaded-by-atc')) {
+  if (sessionStorage.getItem(reloaded-by-atc'''')) {
     sessionStorage.removeItem('reloaded-by-atc');
     openMiniCart(ID);
     console.log('reloaded by atc');
