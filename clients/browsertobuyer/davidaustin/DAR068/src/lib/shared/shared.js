19650bb69861dbc5706b2b8d8b4744fede0dae67
SakibAlIslam <sakibalislam12345@gmail.com>
2025-02-20 21:08:31 +0600

    Fix formatting in shared.js to use single quotes for consistency

--- clients/browsertobuyer/davidaustin/DAR068/src/lib/shared/shared.js
+++ clients/browsertobuyer/davidaustin/DAR068/src/lib/shared/shared.js
@@ -1,6 +1,6 @@
 module.exports = {
-    ID: "DAR068",
-    VARIATION: "1",
-    CLIENT: "browsertobuyer",
-    SITE: "davidaustin"
+    ID: 'DAR068',
+    VARIATION: '1',
+    CLIENT: 'browsertobuyer',
+    SITE: 'davidaustin'
   };
