d909721fdbe80e1fe62b9fcd2f8fda6830b4b998
SakibAlIslam <sakibalislam12345@gmail.com>
2025-02-27 23:52:39 +0600

    pro013 Tracking done

--- clients/browsertobuyer/protonvpn/PRO013/src/lib/components/popover.js
+++ clients/browsertobuyer/protonvpn/PRO013/src/lib/components/popover.js
@@ -3,25 +3,25 @@ const popover = (id) => {
       <div id="whyProtonPopOver" class="${id}__popOver hidden">
           <ul>
               <li>
-                  <a href="#" class="block p-4 text-purple-800 hover:text-purple-500 focus-visible:text-purple-500 hover:bg-purple-25 focus-visible:bg-purple-25">
+                  <a href="#" class="${id}__whyVpnLink block p-4 text-purple-800 hover:text-purple-500 focus-visible:text-purple-500 hover:bg-purple-25 focus-visible:bg-purple-25">
                       <div class="${id}__vpnText font-semibold">Why do I need a VPN?</div>
                       <p class="text-sm text-body">Find out how a VPN can help you</p>
                   </a>
               </li>
               <li>
-                  <a href="/features" class="block p-4 text-purple-800 hover:text-purple-500 focus-visible:text-purple-500 hover:bg-purple-25 focus-visible:bg-purple-25">
+                  <a href="/features" class="${id}__featuresLink block p-4 text-purple-800 hover:text-purple-500 focus-visible:text-purple-500 hover:bg-purple-25 focus-visible:bg-purple-25">
                       <div class="${id}__vpnText font-semibold">All Features</div>
                       <p class="text-sm text-body">Learn how our advanced security and privacy features allow you to browse the web with peace of mind</p>
                   </a>
               </li>
               <li>
-                  <a href="/streaming" class="block p-4 text-purple-800 hover:text-purple-500 focus-visible:text-purple-500 hover:bg-purple-25 focus-visible:bg-purple-25">
+                  <a href="/streaming" class="${id}__streamingLink block p-4 text-purple-800 hover:text-purple-500 focus-visible:text-purple-500 hover:bg-purple-25 focus-visible:bg-purple-25">
                       <div class="${id}__vpnText font-semibold">VPN for Streaming</div>
                       <p class="text-sm text-body">Watch what you want, when you want, from wherever you want with Proton VPN</p>
                   </a>
               </li>
               <li>
-                  <a href="/why-proton-vpn" class="block p-4 text-purple-800 hover:text-purple-500 focus-visible:text-purple-500 hover:bg-purple-25 focus-visible:bg-purple-25">
+                  <a href="/why-proton-vpn" class="${id}__whyProtonVpnLink block p-4 text-purple-800 hover:text-purple-500 focus-visible:text-purple-500 hover:bg-purple-25 focus-visible:bg-purple-25">
                       <div class="${id}__vpnText font-semibold">Why Proton VPN</div>
                       <p class="text-sm text-body">See how we compare with other VPN providers</p>
                   </a>
--- clients/browsertobuyer/protonvpn/PRO013/src/lib/handlers/clickHandler.js
+++ clients/browsertobuyer/protonvpn/PRO013/src/lib/handlers/clickHandler.js
@@ -1,20 +1,26 @@
+/*eslint-disable no-underscore-dangle */
 import { openSidebar, closeSidebar, openSubMenu, closeSubMenu } from '../helpers/utils';
 
 const clickHandler = (id, target) => {
     if (target.closest('#whyProtonBtn')) {
+        const clickedBtn = target.closest('#whyProtonBtn');
         const popover = document.getElementById('whyProtonPopOver');
         const overlay = document.getElementById('popoverOverlay');
         const isHidden = popover.classList.contains('hidden');
 
+        clickedBtn.classList.add(`${id}__whyProtonBtnActive`);
         overlay.classList.toggle('hidden', !isHidden);
         popover.classList.toggle('hidden');
     } else if (target.closest('#popoverOverlay')) {
         const popover = document.getElementById('whyProtonPopOver');
         const overlay = target.closest('#popoverOverlay');
+        const protonBtnElem = document.querySelector('#whyProtonBtn');
 
+        protonBtnElem.classList.remove(`${id}__whyProtonBtnActive`);
         popover.classList.add('hidden');
         overlay.classList.add('hidden');
     } else if (target.closest(`.${id}__hamburgerBtn`)) {
+        window._conv_q.push(['triggerConversion', '100486511']);
         openSidebar();
     } else if (target.closest(`.${id}__closeSidebarBtn`) || target.closest(`.${id}__sideBarMenuOverlay`)) {
         closeSidebar();
@@ -65,6 +71,20 @@ const clickHandler = (id, target) => {
         clickedItem.classList.add(`${id}__active`);
         dropdownWrapper.classList.add(`${id}__countrySelected`);
         countryWrapper.classList.add(`${id}__close`);
+    } else if (target.closest(`.${id}__devices`)) { //TRACKINGS
+        window._conv_q.push(['triggerConversion', '100486504']);
+    } else if (target.closest(`.${id}__locations`)) {
+        window._conv_q.push(['triggerConversion', '100486505']);
+    } else if (target.closest(`.${id}__pricing`)) {
+        window._conv_q.push(['triggerConversion', '100486506']);
+    } else if (target.closest(`.${id}__whyVpnLink`)) {
+        window._conv_q.push(['triggerConversion', '100486507']);
+    } else if (target.closest(`.${id}__featuresLink`)) {
+        window._conv_q.push(['triggerConversion', '100486508']);
+    } else if (target.closest(`.${id}__streamingLink`)) {
+        window._conv_q.push(['triggerConversion', '100486510']);
+    } else if (target.closest(`.${id}__whyProtonVpnLink`)) {
+        window._conv_q.push(['triggerConversion', '100486509']);
     }
 };
 export default clickHandler;
