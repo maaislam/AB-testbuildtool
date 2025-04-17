8bf1ed9783273cb4c84e482a7f11b964a972c12e
maaislam <maaislam@outlook.com>
2025-04-17 14:36:09 +0600

    folder restructure

--- clients/perfectwhitetee/perfectwhitetee/pw001/src/index.js
+++
@@ -1,8 +0,0 @@
-import activate from './lib/experiment';
-import { pollerLite } from './lib/helpers/utils';
-import setup from './lib/services/setup';
-
-if (window.location.pathname.includes('/collections')) {
-    setup();
-    pollerLite(['#CollectionAjaxResult'], activate);
-}
