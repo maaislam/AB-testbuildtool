0befda35a6d04001a7ac8c29fd250dfff5bdae68
raihankabir36850 <raihanku320@gmail.com>
2025-09-02 00:05:06 +0600

    feat: Implement early bird club experiment with product selection and pricing logic
    - Added main experiment logic in experiment.js including product fetching, pricing updates, and event handling.
    - Integrated Swiper for product display with initSwiper.js.
    - Created utility functions in utils.js for DOM polling, product fetching, and price formatting.
    - Set up Google Analytics tracking in gaTracking.js for event tracking.
    - Established initial setup for the experiment in setup.js.
    - Defined shared constants in shared.js for easy access across modules.
    - Added basic styling in shared.scss for the experiment.

---
+++ clients/Moreconversion/clubbearlybird/CB008/src/lib/components/extraUsp.js
@@ -0,0 +1,8 @@
+const extraUsp = (id) => {
+  const html = `
+     <div class="${id}__extraUsp zpa-position--relative zp mcwa-4456140 zpa-el-image"> <div class="zp zpa-image-container mca-4456140" data-id="4456140"> <img src="https://cdn05.zipify.com/g7KZ0BzLeuo3mX82-rp9BOnEXxc=/fit-in/1080x0/66f33d66045b44629c21662407d66f17/earlybird-0-calories.png" sizes="1080px,936px,774px,540px,350px" srcset="https://cdn05.zipify.com/L8o-EwVzB4b9hoZ6Ce-vVgCokqY=/fit-in/350x0/66f33d66045b44629c21662407d66f17/earlybird-0-calories.png 350w, https://cdn05.zipify.com/pdM2YEJkosy2unKHntiNG-WZXcc=/fit-in/540x0/66f33d66045b44629c21662407d66f17/earlybird-0-calories.png 540w, https://cdn05.zipify.com/faqLAngkWOTOl9himBOn9Yy_PJI=/fit-in/774x0/66f33d66045b44629c21662407d66f17/earlybird-0-calories.png 774w, https://cdn05.zipify.com/OfLCeVerNbT4_fe0twO5ukG41vw=/fit-in/936x0/66f33d66045b44629c21662407d66f17/earlybird-0-calories.png 936w, https://cdn05.zipify.com/g7KZ0BzLeuo3mX82-rp9BOnEXxc=/fit-in/1080x0/66f33d66045b44629c21662407d66f17/earlybird-0-calories.png 1080w" width="1000" height="650" class="zpa-img-fluid zp eswb-4456140 ea-4456140"> </div> </div>
+    `;
+  return html.trim();
+};
+
+export default extraUsp;
---
+++ clients/Moreconversion/clubbearlybird/CB008/src/lib/components/flavourItem.js
@@ -0,0 +1,18 @@
+import { flavourData } from '../data/data';
+
+const flavourItem = (item) => {
+  const isFindItem = flavourData.find((flavour) =>
+    flavour.title.toLowerCase().trim().includes(item.toLowerCase().trim())
+  );
+
+  const html = `
+  <div class="flavor-option ${isFindItem.isActive ? 'flavor-active' : ''}" data-key="${item}">
+    <span style="display:none;">${isFindItem.icon}</span>
+    <span>${isFindItem.title}</span>
+  </div>
+  `;
+
+  return html.trim();
+};
+
+export default flavourItem;
